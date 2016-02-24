634e72419a164847188bd94230ee56e728cb6350
Mamoun Gharbi <magharbi@laas.fr>
2016-02-24 14:36:49 +0100

    [wip/libmove3d-planners] update to version 1.4.
    Ameliorating GTP, HRICS and Romeo handling
    
    GTP:
     - restructring files
     - adding json files for object descriptions
     - interfacing with gtp_ros_bridge
     - ameliorating and adding actions
    
    HRICS:
     - integrating hrics to GTP
     - adding new costs
     - adding new robots
     - adding semantics to handle wich cost to use
       and which joints to use (in GTP)
    
    Romeo:
     - Adding romeo to different switch case in GTP
    0

--- libmove3d-planners/depend.mk
+++ libmove3d-planners/depend.mk
@@ -28,7 +28,7 @@ PREFER.libmove3d-planners?=	robotpkg
 
 DEPEND_USE+=		libmove3d-planners
 
-DEPEND_ABI.libmove3d-planners?=		libmove3d-planners>=1.10.1
+DEPEND_ABI.libmove3d-planners?=		libmove3d-planners>=1.4.0
 DEPEND_DIR.libmove3d-planners?=		../../wip/libmove3d-planners
 
 SYSTEM_SEARCH.libmove3d-planners=\

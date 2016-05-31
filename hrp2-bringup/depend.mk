67c3f70a813f74437517d8be462459f7674954ec
Anthony Mallet <anthony.mallet@laas.fr>
2016-05-31 15:39:04 +0200

    [wip/hrp2-bringup] Fix PLIST
    * Add missing entries to PLIST
    * Fix CATEGORY
    * Drop unused variables

--- hrp2-bringup/depend.mk
+++ hrp2-bringup/depend.mk
@@ -9,7 +9,7 @@ ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		hrp2-bringup
 endif
 
-ifeq (+,$(HRP2_BRINGUP_DEPEND_MK)) # -------------------------------------------
+ifeq (+,$(HRP2_BRINGUP_DEPEND_MK)) # ---------------------------------------
 
 PREFER.hrp2-bringup?=	robotpkg
 
@@ -17,11 +17,10 @@ SYSTEM_SEARCH.hrp2-bringup=\
 	'lib/pkgconfig/hrp2_bringup.pc:/Version/s/[^0-9.]//gp'
 
 DEPEND_USE+=		hrp2-bringup
-ROS_DEPEND_USER+=	hrp2-bringup
 
 DEPEND_ABI.hrp2-bringup?=	hrp2-bringup>=1.0.0
 DEPEND_DIR.hrp2-bringup?=	../hrp2-bringup
 
-endif # HRP2_14_DESCRIPTION_DEPEND_MK -------------------------------------------------
+endif # HRP2_14_DESCRIPTION_DEPEND_MK --------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

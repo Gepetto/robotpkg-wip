82e2bd254647f954fce20303894807d85e5b1ea0
Anthony Mallet <anthony.mallet@laas.fr>
2016-06-01 17:08:00 +0200

    [wip/pinocchio] Fix a number of issues in the package
    * Disable parallel builds as g++ processes consumes loads of memory
    * Fix PLIST
    * Fix DEPEND_ABIs: drop useless requirements on assimp, add minimum 3.0 version
      on urdfdom
    * Drop specific user settings (PREFER.*)
    * Explicitly set cmake build options
    * Sort dependencies
    
    patch-aa: ensure that cmake does not find unwanted optional dependencies
    patch-ab: fix a quoting issue in a cmakefile
    patch-ac: fix build with assimp-2

--- pinocchio/depend.mk
+++ pinocchio/depend.mk
@@ -1,5 +1,5 @@
-# robotpkg depend.mk for:	wip/pinocchio
-# Created:			Olivier Stasse, Feb 4th 2016
+# robotpkg depend.mk for:	math/pinocchio
+# Created:			Olivier Stasse on Thu, 4 Feb 2016
 #
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
@@ -9,18 +9,18 @@ ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		pinocchio
 endif
 
-ifeq (+,$(PINOCCHIO_DEPEND_MK)) # -------------------------------------------
+ifeq (+,$(PINOCCHIO_DEPEND_MK)) # ------------------------------------------
 
 PREFER.pinocchio?=	robotpkg
 
 SYSTEM_SEARCH.pinocchio=\
-	'lib/pkgconfig/pinocchio.pc:/Version/s/[^0-9.]//gp'
+  'lib/pkgconfig/pinocchio.pc:/Version/s/[^0-9.]//gp'
 
 DEPEND_USE+=		pinocchio
 
 DEPEND_ABI.pinocchio?=	pinocchio>=1.1.2
 DEPEND_DIR.pinocchio?=	../../wip/pinocchio
 
-endif # PINOCCHIO_DEPEND_MK -------------------------------------------------
+endif # PINOCCHIO_DEPEND_MK ------------------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

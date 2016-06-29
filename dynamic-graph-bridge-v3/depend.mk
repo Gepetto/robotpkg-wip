33f7ce3f0db972c8961408f169f913e4b6134140
Rohan Budhiraja <budhiraja@laas.fr>
2016-06-29 19:25:06 +0200

    [wip/dynamic-graph-bridge-v3] correct PLIST and depend.mk

--- dynamic-graph-bridge-v3/depend.mk
+++ dynamic-graph-bridge-v3/depend.mk
@@ -22,11 +22,17 @@ DEPEND_ABI.dynamic-graph-bridge-v3.groovy?=	dynamic-graph-bridge-v3>=3.0.0
 DEPEND_ABI.dynamic-graph-bridge-v3.hydro?=	dynamic-graph-bridge-v3>=3.0.0
 
 SYSTEM_SEARCH.dynamic-graph-bridge-v3=\
-  'include/dynamic_graph_bridge/config.h'\
-  'lib/pkgconfig/dynamic_graph_bridge.pc'\
-  'lib/librobot_model.so'\
-  'share/dynamic_graph_bridge/robot_pose_publisher'\
+  'include/dynamic_graph_bridge/config.hh'				\
+  'lib/pkgconfig/dynamic_graph_bridge.pc:/Version/s/[^0-9.]//gp'	\
+  'lib/plugin/robot_model.so'						\
+  'share/dynamic_graph_bridge/robot_pose_publisher'			\
   '${PYTHON_SITELIB}/dynamic_graph/ros/ros.py'
+
+
+# headers leak the ros dependency ...
+include ../../middleware/ros-comm/depend.mk
+
+include ../../mk/sysdep/python.mk
 
 endif # ROS_REALTIMETOOLS_DEPEND_MK --------------------------------------------
 

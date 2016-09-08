0482bf129e3742753fd5929d8c021423ba9f80b4
Rohan Budhiraja <budhiraja@laas.fr>
2016-09-08 17:41:49 +0200

    [wip/sot-dyninv-v3] Create sot-dyninv-v3

---
+++ sot-dyninv-v3/depend.mk
@@ -0,0 +1,27 @@
+# robotpkg depend.mk for:	wip/sot-dyninv-v3
+# Created:			Rohan Budhiraja on Thu, 08 Sep 2016
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+SOT_DYNINV_V3_DEPEND_MK:=	${SOT_DYNINV_V3_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		sot-dyninv-v3
+endif
+
+ifeq (+,$(SOT_DYNINV_v3_DEPEND_MK)) # -------------------------------------------
+
+PREFER.sot-dyninv-v3?=	robotpkg
+
+SYSTEM_SEARCH.sot-dyninv-v3=\
+	include/sot-dyninv/config.h				\
+	'lib/pkgconfig/sot-dyninv.pc:/Version/s/[^0-9.]//gp'
+
+DEPEND_USE+=		sot-dyninv-v3
+
+DEPEND_ABI.sot-dyninv?=	sot-dyninv-v3>=3.0.0
+DEPEND_DIR.sot-dyninv?=	../../wip/sot-dyninv-v3
+
+endif # SOT_DYNINV_v3_DEPEND_MK -------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

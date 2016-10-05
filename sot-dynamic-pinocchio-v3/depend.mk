b954084a426797b9f98a6b547d83a08735bd4149
Rohan Budhiraja <budhiraja@laas.fr>
2016-10-05 08:59:59 +0200

    [wip/sot-dynamic-pinocchio-v3] Update for pinocchio 1.2.0

--- sot-dynamic-pinocchio-v3/depend.mk
+++ sot-dynamic-pinocchio-v3/depend.mk
@@ -20,7 +20,7 @@ SYSTEM_SEARCH.sot-dynamic-pinocchio-v3=\
 
 DEPEND_USE+=		sot-dynamic-pinocchio-v3
 
-DEPEND_ABI.sot-dynamic-pinocchio-v3?=	sot-dynamic-pinocchio-v3>=3.1
+DEPEND_ABI.sot-dynamic-pinocchio-v3?=	sot-dynamic-pinocchio-v3>=3.1.1
 DEPEND_DIR.sot-dynamic-pinocchio-v3?=	../../wip/sot-dynamic-pinocchio-v3
 
 endif # SOT_DYNAMIC_DEPEND_MK --------------------------------------------

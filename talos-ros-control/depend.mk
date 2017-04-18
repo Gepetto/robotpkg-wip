b417b50c77bbd0eaa28e32324dd70a5b97db58af
Olivier Stasse <ostasse@laas.fr>
2017-04-18 06:19:10 +0200

    [wip/talos-ros-controllers] Fix dependencies not reflecting the name changes. Change depend.mk accordingly.

--- talos-ros-control/depend.mk
+++ talos-ros-control/depend.mk
@@ -19,10 +19,10 @@ DEPEND_ABI.talos-ros-control?=	talos-ros-control>=0.2
 DEPEND_DIR.talos-ros-control?=	../../wip/talos-ros-control
 
 SYSTEM_SEARCH.talos-ros-control=\
-  'include/controller_manager/controller_loader.h'		\
-  'lib/libcontroller_manager.so'				\
-  'share/ros_control/package.xml:/<version>/s/[^0-9.]//gp'	\
-  'lib/pkgconfig/controller_interface.pc:/Version/s/[^0-9.]//gp'
+  'include/talos_controller_manager/controller_loader.h'		\
+  'lib/libtalos_controller_manager.so'				\
+  'share/talos_ros_control/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/talos_controller_interface.pc:/Version/s/[^0-9.]//gp'
 
 endif # TALOS_ROS_CONTROL_DEPEND_MK ----------------------------------------
 

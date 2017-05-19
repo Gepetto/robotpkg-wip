e6cc7fec278c8817167bafbb677386aa39f8d948
Anthony Mallet <anthony.mallet@laas.fr>
2017-05-19 15:21:42 +0200

    [wip/talos-ros-control] Add missing dependency on ros-rqt, fix SYSTEM_SEARCH
    SYSTEM_SEARCH should hold the *Config.cmake files so that mk/sysdep/cmake.mk
    automatically add *_DIR variable based on the resolved dependency paths.
    
    Bump PKGREVISION

--- talos-ros-control/depend.mk
+++ talos-ros-control/depend.mk
@@ -20,9 +20,20 @@ DEPEND_DIR.talos-ros-control?=	../../wip/talos-ros-control
 
 SYSTEM_SEARCH.talos-ros-control=\
   'include/talos_controller_manager/controller_loader.h'		\
-  'lib/libtalos_controller_manager.so'				\
+  'lib/libtalos_controller_manager.so'					\
   'share/talos_ros_control/package.xml:/<version>/s/[^0-9.]//gp'	\
-  'lib/pkgconfig/talos_controller_interface.pc:/Version/s/[^0-9.]//gp'
+  $(foreach _,								\
+	talos_controller_interface					\
+	rqt_talos_controller_manager					\
+	talos_controller_interface					\
+	talos_controller_manager					\
+	talos_controller_manager_msgs					\
+	talos_controller_manager_tests					\
+	talos_hardware_interface					\
+	talos_joint_limits_interface					\
+	talos_transmission_interface,					\
+  'share/$_/cmake/$_Config.cmake'					\
+  'lib/pkgconfig/$_.pc:/Version/s/[^0-9.]//gp')
 
 endif # TALOS_ROS_CONTROL_DEPEND_MK ----------------------------------------
 

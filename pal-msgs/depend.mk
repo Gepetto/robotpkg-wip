cb53743ed90349bcf64afaa945b3d66ed8664d65
Olivier Stasse <ostasse@laas.fr>
2017-06-20 15:44:29 +0200

    [wip/pal-msgs] Fix depend.mk to find properly the cmake file.

--- pal-msgs/depend.mk
+++ pal-msgs/depend.mk
@@ -22,7 +22,27 @@ DEPEND_DIR.pal-msgs?=\
 
 SYSTEM_SEARCH.pal-msgs=\
   'include/pal_behaviour_msgs/BehaviourArgument.h'	\
-  'share/pal_behaviour_msgs/package.xml:/<version>/s/[^0-9.]//gp'
+  'share/pal_behaviour_msgs/package.xml:/<version>/s/[^0-9.]//gp'\
+  $(foreach _,								\
+	pal_behaviour_msgs						\
+	pal_common_msgs							\
+	pal_control_msgs						\
+	pal_detection_msgs						\
+	pal_device_msgs							\
+	pal_interaction_msgs						\
+	pal_motion_model_msgs						\
+	pal_multirobot_msgs						\
+	pal_navigation_msgs						\
+	pal_tablet_msgs							\
+	pal_video_recording_msgs					\
+	pal_vision_msgs							\
+	pal_visual_localization_msgs					\
+	pal_walking_msgs						\
+	pal_web_msgs							\
+	pal_wifi_localization_msgs,					\
+  'share/$_/cmake/$_Config.cmake'					\
+  'lib/pkgconfig/$_.pc:/Version/s/[^0-9.]//gp')
+
 
 endif # PAL_MSGS_DEPEND_MK ---------------------------------------
 

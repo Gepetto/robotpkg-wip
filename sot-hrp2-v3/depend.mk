21f6b8beabe0351603a15a7be22f488f48d04ccd
Olivier Stasse <ostasse@laas.fr>
2017-07-24 18:39:04 +0200

    [wip/sot-hrp2-v3] Fix wrong variables in depend.mk This bug prevented correct inclusion of sot-hrp2-v3 in other packages dependencies.

--- sot-hrp2-v3/depend.mk
+++ sot-hrp2-v3/depend.mk
@@ -9,7 +9,7 @@ ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		sot-hrp2-v3
 endif
 
-ifeq (+,$(SOTH_DEPEND_MK)) # -------------------------------------------
+ifeq (+,$(SOT_HRP2_V3_DEPEND_MK)) # -------------------------------------------
 
 PREFER.sot-hrp2-v3?=	robotpkg
 
@@ -22,6 +22,6 @@ DEPEND_USE+=		sot-hrp2-v3
 DEPEND_ABI.sot-hrp2-v3?=	sot-hrp2-v3>=2.0.0
 DEPEND_DIR.sot-hrp2-v3?=	../../wip/sot-hrp2-v3
 
-endif # SOT_HRP2_DEPEND_MK -------------------------------------------------
+endif # SOT_HRP2_V3_DEPEND_MK -------------------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

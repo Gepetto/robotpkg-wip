c2da77e57c351630e390bc787ba3f59fa460b1ec
Joseph Mirabel <jmirabel@laas.fr>
2017-09-27 11:06:01 +0200

    [wip/talos-metapkg-ros-control-sot] Replace talos-ros-control-sot by talos-metapkg-ros-control-sot

--- talos-metapkg-ros-control-sot/depend.mk
+++ talos-metapkg-ros-control-sot/depend.mk
@@ -1,4 +1,4 @@
-# robotpkg depend.mk for:	wip/talos-ros-control-sot
+# robotpkg depend.mk for:	wip/talos-metapkg-ros-control-sot
 # Created:			Olivier Stasse on Wed, 5 May 2017
 #
 
@@ -6,19 +6,19 @@ DEPEND_DEPTH:=			${DEPEND_DEPTH}+
 TALOS_ROS_CONTROL_SOT_DEPEND_MK:=	${TALOS_ROS_CONTROL_SOT_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=			talos-ros-control-sot
+DEPEND_PKG+=			talos-metapkg-ros-control-sot
 endif
 
 ifeq (+,$(TALOS_ROS_CONTROL_SOT_DEPEND_MK)) # ----------------------------------
 
-PREFER.talos-ros-control-sot?=	robotpkg
+PREFER.talos-metapkg-ros-control-sot?=	robotpkg
 
-DEPEND_USE+=			talos-ros-control-sot
+DEPEND_USE+=			talos-metapkg-ros-control-sot
 
-DEPEND_ABI.talos-ros-control-sot?=	talos-ros-control-sot>=0.0.1
-DEPEND_DIR.talos-ros-control-sot?=	../../wip/talos-ros-control-sot
+DEPEND_ABI.talos-metapkg-ros-control-sot?=	talos-metapkg-ros-control-sot>=0.0.1
+DEPEND_DIR.talos-metapkg-ros-control-sot?=	../../wip/talos-metapkg-ros-control-sot
 
-SYSTEM_SEARCH.talos-ros-control-sot=\
+SYSTEM_SEARCH.talos-metapkg-ros-control-sot=\
   'lib/libtalos_rcsot_controller.so'				\
   'share/talos_roscontrol_sot/package.xml:/<version>/s/[^0-9.]//gp'	\
   'lib/pkgconfig/talos_roscontrol_sot.pc:/Version/s/[^0-9.]//gp'

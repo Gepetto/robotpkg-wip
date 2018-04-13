02e54be38881af441df62b522a68fdace93bde15
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-04-13 17:05:15 +0200

    [wip/pal-gazebo-worlds] new package

---
+++ pal-gazebo-worlds/depend.mk
@@ -0,0 +1,25 @@
+# robotpkg depend.mk for:	wip/pal-gazebo-worlds
+# Created:			Guilhem Saurel on fri, 13 Apr 2018
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+PAL_GAZEBO_WORLDS_DEPEND_MK:=	${PAL_GAZEBO_WORLDS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			pal-gazebo-worlds
+endif
+
+ifeq (+,$(PAL_GAZEBO_WORLDS_DEPEND_MK)) # ---------------------------------
+
+PREFER.pal-gazebo-worlds?=	robotpkg
+
+DEPEND_USE+=			pal-gazebo-worlds
+
+DEPEND_ABI.pal-gazebo-worlds?=\
+	pal-gazebo-worlds>=2.0.4
+DEPEND_DIR.pal-gazebo-worlds?=\
+	../../wip/pal-gazebo-worlds
+
+endif # PAL_GAZEBO_WORLDS_DEPEND_MK ---------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

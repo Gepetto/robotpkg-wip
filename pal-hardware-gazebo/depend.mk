f13cc0179c9c519756fc5304b09734892c5b90f8
Olivier Stasse <ostasse@laas.fr>
2018-04-28 10:12:14 +0200

    [wip/talos-pal-hardware-gazebo]->[wip/pal-hardware-gazebo]

---
+++ pal-hardware-gazebo/depend.mk
@@ -0,0 +1,31 @@
+# robotpkg depend.mk for:	wip/pal-hardware-gazebo
+# Created:			Olivier Stasse on Thu, 30 Mar 2017
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+PAL_HARDWARE_GAZEBO_DEPEND_MK:=	${PAL_HARDWARE_GAZEBO_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			pal-hardware-gazebo
+endif
+
+ifeq (+,$(PAL_HARDWARE_GAZEBO_DEPEND_MK)) # ---------------------------------
+
+PREFER.pal-hardware-gazebo?=	robotpkg
+
+DEPEND_USE+=			pal-hardware-gazebo
+
+DEPEND_ABI.pal-hardware-gazebo?=\
+	pal-hardware-gazebo>=0.0.9
+DEPEND_DIR.pal-hardware-gazebo?=\
+	../../wip/pal-hardware-gazebo
+
+SYSTEM_SEARCH.pal-hardware-gazebo=\
+  'include/pal_hardware_gazebo/pal_hardware_gazebo.h'	\
+  'share/pal_hardware_gazebo/cmake/pal_hardware_gazeboConfig.cmake' \
+  'share/pal_hardware_gazebo/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/pal_hardware_gazebo.pc:/Version/s/[^0-9.]//gp'
+
+endif # PAL_HARDWARE_GAZEBO_DEPEND_MK ---------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

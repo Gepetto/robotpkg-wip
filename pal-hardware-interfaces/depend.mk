82adcfcfc78a435b61c8079a9c12fa79425f41f1
Olivier Stasse <ostasse@laas.fr>
2018-04-28 10:13:16 +0200

    [wip/pal-hardware-interfaces] Fix pkgname and dependencies.

--- pal-hardware-interfaces/depend.mk
+++ pal-hardware-interfaces/depend.mk
@@ -1,31 +1,31 @@
-# robotpkg depend.mk for:	wip/talos-pal-hardware-interfaces
+# robotpkg depend.mk for:	wip/pal-hardware-interfaces
 # Created:			Olivier Stasse on Thu, 30 Mar 2017
 #
 
 DEPEND_DEPTH:=			${DEPEND_DEPTH}+
-TALOS_PAL_HWIFACES_DEPEND_MK:=	${TALOS_PAL_HWIFACES_DEPEND_MK}+
+PAL_HWIFACES_DEPEND_MK:=	${PAL_HWIFACES_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=			talos-pal-hardware-interfaces
+DEPEND_PKG+=			pal-hardware-interfaces
 endif
 
-ifeq (+,$(TALOS_PAL_HWIFACES_DEPEND_MK)) # ---------------------------------
+ifeq (+,$(PAL_HWIFACES_DEPEND_MK)) # ---------------------------------
 
-PREFER.talos-pal-hardware-interfaces?=	robotpkg
+PREFER.pal-hardware-interfaces?=	robotpkg
 
-DEPEND_USE+=			talos-pal-hardware-interfaces
+DEPEND_USE+=			pal-hardware-interfaces
 
-DEPEND_ABI.talos-pal-hardware-interfaces?=\
-	talos-pal-hardware-interfaces>=0.0.2
-DEPEND_DIR.talos-pal-hardware-interfaces?=\
-	../../wip/talos-pal-hardware-interfaces
+DEPEND_ABI.pal-hardware-interfaces?=\
+	pal-hardware-interfaces>=0.0.3
+DEPEND_DIR.pal-hardware-interfaces?=\
+	../../wip/pal-hardware-interfaces
 
-SYSTEM_SEARCH.talos-pal-hardware-interfaces=\
-  'include/talos_pal_hardware_interfaces/actuator_temperature_interface.h'	\
-  'share/talos_pal_hardware_interfaces/cmake/talos_pal_hardware_interfacesConfig.cmake'	\
-  'share/talos_pal_hardware_interfaces/package.xml:/<version>/s/[^0-9.]//gp'	\
-  'lib/pkgconfig/talos_pal_hardware_interfaces.pc:/Version/s/[^0-9.]//gp'
+SYSTEM_SEARCH.pal-hardware-interfaces=\
+  'include/pal_hardware_interfaces/actuator_temperature_interface.h'	\
+  'share/pal_hardware_interfaces/cmake/pal_hardware_interfacesConfig.cmake'	\
+  'share/pal_hardware_interfaces/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/pal_hardware_interfaces.pc:/Version/s/[^0-9.]//gp'
 
-endif # TALOS_PAL_HWIFACES_DEPEND_MK ---------------------------------------
+endif # PAL_HWIFACES_DEPEND_MK ---------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

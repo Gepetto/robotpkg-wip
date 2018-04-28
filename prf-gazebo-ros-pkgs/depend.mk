f2f39d047358db29dade4ea254c2b6193ec101a8
Olivier Stasse <ostasse@laas.fr>
2018-04-28 10:13:43 +0200

    [wip/prf-gazebo-ros-pkgs] Fix list of installed files + dependencies.

--- prf-gazebo-ros-pkgs/depend.mk
+++ prf-gazebo-ros-pkgs/depend.mk
@@ -6,23 +6,23 @@ DEPEND_DEPTH:=				${DEPEND_DEPTH}+
 GAZEBO_ROS_PKGS_DEPEND_MK:=	${GAZEBO_ROS_PKGS_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=				gazebo_ros_pkgs
+DEPEND_PKG+=				prf-gazebo-ros-pkgs
 endif
 
 ifeq (+,$(GAZEBO_ROS_PKGS_DEPEND_MK)) # ---------------------------
 
-PREFER.gazebo_ros_pkgs?=	robotpkg
+PREFER.prf-gazebo-ros-pkgs?=	robotpkg
 
-DEPEND_USE+=				gazebo_ros_pkgs
+DEPEND_USE+=				prf-gazebo-ros-pkgs
 
-DEPEND_ABI.gazebo_ros_pkgs?=	gazebo_ros_pkgs>=2.6.5
-DEPEND_DIR.gazebo_ros_pkgs?=	../../wip/gazebo_ros_pkgs
+DEPEND_ABI.prf-gazebo-ros-pkgs?=	prf-gazebo-ros-pkgs>=2.6.5
+DEPEND_DIR.prf-gazebo-ros-pkgs?=	../../wip/prf-gazebo-ros-pkgs
 
-SYSTEM_SEARCH.gazebo_ros_pkgs=\
-  'include/gazebo_ros_pkgs/gazebo_ros_control_plugin.h'	\
-  'share/gazebo_ros_pkgs/cmake/gazebo_ros_pkgsConfig.cmake' \
-  'share/gazebo_ros_pkgs/package.xml:/<version>/s/[^0-9.]//gp'	\
-  'lib/pkgconfig/gazebo_ros_pkgs.pc:/Version/s/[^0-9.]//gp'
+SYSTEM_SEARCH.prf-gazebo-ros-pkgs=\
+  'include/gazebo_ros_control/gazebo_ros_control_plugin.h'	\
+  'share/gazebo_ros_control/cmake/gazebo_ros_controlConfig.cmake' \
+  'share/gazebo_ros_control/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/gazebo_ros_control.pc:/Version/s/[^0-9.]//gp'
 
 endif # GAZEBO_ROS_PKGS_DEPEND_MK ---------------------------------
 

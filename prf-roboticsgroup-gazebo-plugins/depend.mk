aa98fcf75a7b90e12b86b1187d00d48ce69c6ccf
Olivier Stasse <ostasse@laas.fr>
2018-04-29 15:32:51 +0200

    [wip/talos-roboticsgroup-gazebo-plugins] -> [wip/prf-roboticsgroup-gazebo-plugins]

---
+++ prf-roboticsgroup-gazebo-plugins/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	wip/prf-roboticsgroup-gazebo-plugins
+# Created:			Olivier Stasse on Thu, 4 May 2017
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+PRF_ROBOTICSGRP_GAZEBO_PLUGINS_DEPEND_MK:=	${PRF_ROBOTICSGRP_GAZEBO_PLUGINS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			prf-roboticsgroup-gazebo-plugins
+endif
+
+ifeq (+,$(PRF_ROBOTICSGRP_GAZEBO_PLUGINS_DEPEND_MK)) # ----------------------------------
+
+PREFER.prf-roboticsgroup-gazebo-plugins?=	robotpkg
+
+DEPEND_USE+=			prf-roboticsgroup-gazebo-plugins
+
+DEPEND_ABI.prf-roboticsgroup-gazebo-plugins?=	prf-roboticsgroup-gazebo-plugins>=0.0.3
+DEPEND_DIR.prf-roboticsgroup-gazebo-plugins?=	../../wip/prf-roboticsgroup-gazebo-plugins
+
+SYSTEM_SEARCH.prf-roboticsgroup-gazebo-plugins=\
+  'lib/libroboticsgroup_gazebo_disable_link_plugin.so' \
+  'share/roboticsgroup_gazebo_plugins/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/roboticsgroup_gazebo_plugins.pc:/Version/s/[^0-9.]//gp'
+
+endif # PRF_ROBOTICSGRP_GAZEBO_PLUGINS_DEPEND_MK ----------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

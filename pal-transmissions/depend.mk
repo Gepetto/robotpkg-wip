f6d3a3920037f82f4d0f00def62000c8a10361e9
Olivier Stasse <ostasse@laas.fr>
2018-04-29 14:56:56 +0200

    [wip/talos-pal-transmissions] -> [wip/pal-transmissions] Change naming.

---
+++ pal-transmissions/depend.mk
@@ -0,0 +1,30 @@
+# robotpkg depend.mk for:	wip/pal-transmissions
+# Created:			Olivier Stasse on Thu, 18 Apr 2017
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+PAL_TRANSMISSIONS_DEPEND_MK:=	${PAL_TRANSMISSIONS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			pal-transmissions
+endif
+
+ifeq (+,$(PAL_TRANSMISSIONS_DEPEND_MK)) # --------------------------------
+
+PREFER.pal-transmissions?=	robotpkg
+
+DEPEND_USE+=			pal-transmissions
+
+DEPEND_ABI.pal-transmissions?=\
+	pal-transmissions>=0.2.3
+DEPEND_DIR.pal-transmissions?=\
+	../../wip/pal-transmissions
+
+SYSTEM_SEARCH.pal-transmissions=\
+  'include/pal_transmissions/actuator_temperature_interface.h'	\
+  'share/pal_hardware_interfaces/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/pal_hardware_interfaces.pc:/Version/s/[^0-9.]//gp'
+
+endif # PAL_TRANSMISSIONS_DEPEND_MK ---------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

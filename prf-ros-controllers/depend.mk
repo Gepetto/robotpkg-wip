a813fe9afdeebf12597cbf9aa4d3fd32d283da8d
Olivier Stasse <ostasse@laas.fr>
2018-05-06 09:56:26 +0200

    [wip/prf-ros-controllers] Fix depend.mk

--- prf-ros-controllers/depend.mk
+++ prf-ros-controllers/depend.mk
@@ -1,30 +1,30 @@
-# robotpkg depend.mk for:	wip/talos-ros-controllers
+# robotpkg depend.mk for:	wip/ros-controllers
 # Created:			Olivier Stasse on Wed, 29 Mar 2017
 #
 
 DEPEND_DEPTH:=			${DEPEND_DEPTH}+
-TALOS_ROS_CONTROLLERS_DEPEND_MK:=	${TALOS_ROS_CONTROLLERS_DEPEND_MK}+
+ROS_CONTROLLERS_DEPEND_MK:=	${ROS_CONTROLLERS_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=			talos-ros-controllers
+DEPEND_PKG+=			ros-controllers
 endif
 
-ifeq (+,$(TALOS_ROS_CONTROLLERS_DEPEND_MK)) # ----------------------------------
+ifeq (+,$(ROS_CONTROLLERS_DEPEND_MK)) # ----------------------------------
 
-PREFER.talos-ros-controllers?=	robotpkg
+PREFER.ros-controllers?=	robotpkg
 
-DEPEND_USE+=			talos-ros-controllers
+DEPEND_USE+=			ros-controllers
 
-DEPEND_ABI.talos-ros-controllers?=	talos-ros-controllers>=0.2.8
-DEPEND_DIR.talos-ros-controllers?=	../../wip/talos-ros-controllers
+DEPEND_ABI.ros-controllers?=	ros-controllers>=0.2.8
+DEPEND_DIR.ros-controllers?=	../../wip/ros-controllers
 
-SYSTEM_SEARCH.talos-ros-controllers=\
-  'include/talos_joint_state_controller/joint_state_controller.h'		\
-  'lib/libtalos_joint_state_controller.so'				\
-  'share/talos_joint_state_controller/package.xml:/<version>/s/[^0-9.]//gp'	\
-  'lib/pkgconfig/talos_joint_state_controller.pc:/Version/s/[^0-9.]//gp'  \
-  'share/talos_controller_interface/cmake/talos_controller_interfaceConfig.cmake'
+SYSTEM_SEARCH.ros-controllers=\
+  'include/joint_state_controller/joint_state_controller.h'		\
+  'lib/libjoint_state_controller.so'				\
+  'share/joint_state_controller/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/joint_state_controller.pc:/Version/s/[^0-9.]//gp'  \
+  'share/controller_interface/cmake/controller_interfaceConfig.cmake'
 
-endif # TALOS_ROS_CONTROLLERS_DEPEND_MK ----------------------------------------
+endif # ROS_CONTROLLERS_DEPEND_MK ----------------------------------------
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

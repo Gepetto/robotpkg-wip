83576cdd7cf22b1864448a8851924c66300f7dac
Olivier Stasse <ostasse@laas.fr>
2018-05-07 17:46:29 +0200

    [wip/prf-ros-controllers] Fix depend.mk

--- prf-ros-controllers/depend.mk
+++ prf-ros-controllers/depend.mk
@@ -6,19 +6,19 @@ DEPEND_DEPTH:=			${DEPEND_DEPTH}+
 ROS_CONTROLLERS_DEPEND_MK:=	${ROS_CONTROLLERS_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=			ros-controllers
+DEPEND_PKG+=			prf-ros-controllers
 endif
 
 ifeq (+,$(ROS_CONTROLLERS_DEPEND_MK)) # ----------------------------------
 
 PREFER.ros-controllers?=	robotpkg
 
-DEPEND_USE+=			ros-controllers
+DEPEND_USE+=			prf-ros-controllers
 
-DEPEND_ABI.ros-controllers?=	ros-controllers>=0.2.8
-DEPEND_DIR.ros-controllers?=	../../wip/ros-controllers
+DEPEND_ABI.prf-ros-controllers?=	prf-ros-controllers>=0.2.8
+DEPEND_DIR.prf-ros-controllers?=	../../wip/prf-ros-controllers
 
-SYSTEM_SEARCH.ros-controllers=\
+SYSTEM_SEARCH.prf-ros-controllers=\
   'include/joint_state_controller/joint_state_controller.h'		\
   'lib/libjoint_state_controller.so'				\
   'share/joint_state_controller/package.xml:/<version>/s/[^0-9.]//gp'	\

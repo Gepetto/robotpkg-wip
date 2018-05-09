b30bce2b8c5dcb153220aa3b889d2d5faec3126e
Olivier Stasse <ostasse@laas.fr>
2018-05-09 16:38:53 +0200

    [wip/rviz-plugin-covariance] Fix depend.mk with rviz-plugin-covariance.

--- rviz-plugin-covariance/depend.mk
+++ rviz-plugin-covariance/depend.mk
@@ -16,7 +16,7 @@ PREFER.rviz-plugin-covariance?=	robotpkg
 DEPEND_USE+=			rviz-plugin-covariance
 
 DEPEND_ABI.rviz-plugin-covariance?=	rviz-plugin-covariance>=0.0.6
-DEPEND_DIR.rviz-plugin-covariance?=	../../wip/pmb2-robot
+DEPEND_DIR.rviz-plugin-covariance?=	../../wip/rviz-plugin-covariance
 
 SYSTEM_SEARCH.pmb2-robot=\
   'share/rviz_plugin_covariance/package.xml:/<version>/s/[^0-9.]//gp'	\

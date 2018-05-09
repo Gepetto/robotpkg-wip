55afa1fdfcbf53758192f56847b6860641b3d39a
Olivier Stasse <ostasse@laas.fr>
2018-05-09 19:01:50 +0200

    [wip/tiago-robot] Adds the TIAGO description metapackage.

---
+++ tiago-robot/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	wip/tiago-robot
+# Created:			Olivier Stasse on Wed, 29 Mar 2017
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+TIAGO_ROBOT_DEPEND_MK:=	${TIAGO_ROBOT_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			tiago-robot
+endif
+
+ifeq (+,$(TIAGO_ROBOT_DEPEND_MK)) # ----------------------------------
+
+PREFER.tiago-robot?=	robotpkg
+
+DEPEND_USE+=			tiago-robot
+
+DEPEND_ABI.tiago-robot?=	tiago-robot>=0.1.7
+DEPEND_DIR.tiago-robot?=	../../wip/tiago-robot
+
+SYSTEM_SEARCH.tiago-robot=\
+  'share/tiago_robot/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/tiago_robot.pc:/Version/s/[^0-9.]//gp'  \
+  'share/tiago_robot/cmake/tiago_robotConfig.cmake'
+
+endif # TIAGO_ROBOT_DEPEND_MK ----------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

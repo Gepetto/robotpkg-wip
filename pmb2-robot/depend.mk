6cfc17419d6e982f490b42a4510074a8a1071623
Olivier Stasse <ostasse@laas.fr>
2018-05-09 10:38:06 +0200

    [wip/pmb2-robot] Add PMB2 PAL mobile base robot.

---
+++ pmb2-robot/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	wip/pmb2-robot
+# Created:			Olivier Stasse on Wed, 29 Mar 2017
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+PMB2_ROBOT_DEPEND_MK:=	${PMB2_ROBOT_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			pmb2-robot
+endif
+
+ifeq (+,$(PMB2_ROBOT_DEPEND_MK)) # ----------------------------------
+
+PREFER.pmb2-robot?=	robotpkg
+
+DEPEND_USE+=			pmb2-robot
+
+DEPEND_ABI.pmb2-robot?=	pmb2-robot>=1.1.8
+DEPEND_DIR.pmb2-robot?=	../../wip/pmb2-robot
+
+SYSTEM_SEARCH.pmb2-robot=\
+  'share/pmb2_robot/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/pmb2_robot.pc:/Version/s/[^0-9.]//gp'  \
+  'share/pmb2_robot/cmake/pmb2_robotConfig.cmake'
+
+endif # PMB2_ROBOT_DEPEND_MK ----------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

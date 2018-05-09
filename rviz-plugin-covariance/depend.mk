5d27ac501d8a5611a6c158256ed5b056c6da683e
Olivier Stasse <ostasse@laas.fr>
2018-05-09 11:48:10 +0200

    [wip/rviz-plugin-covariance] Plugin providing covariance display.

---
+++ rviz-plugin-covariance/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	wip/rviz-plugin-covariance
+# Created:			Olivier Stasse on Wed, 29 Mar 2017
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+RVIZ_PLUGIN_COVARIANCE_DEPEND_MK:=	${RVIZ_PLUGIN_COVARIANCE_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			rviz-plugin-covariance
+endif
+
+ifeq (+,$(RVIZ_PLUGIN_COVARIANCE_DEPEND_MK)) # ----------------------------------
+
+PREFER.rviz-plugin-covariance?=	robotpkg
+
+DEPEND_USE+=			rviz-plugin-covariance
+
+DEPEND_ABI.rviz-plugin-covariance?=	rviz-plugin-covariance>=0.0.6
+DEPEND_DIR.rviz-plugin-covariance?=	../../wip/pmb2-robot
+
+SYSTEM_SEARCH.pmb2-robot=\
+  'share/rviz_plugin_covariance/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/rviz_plugin_covariance.pc:/Version/s/[^0-9.]//gp'  \
+  'share/rviz_plugin_covariance/cmake/rviz_plugin_covarianceConfig.cmake'
+
+endif # RVIZ_PLUGIN_COVARIANCE_DEPEND_MK ----------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

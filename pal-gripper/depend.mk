6c6eed4ba82e02e281b464933a149db445421986
Olivier Stasse <ostasse@laas.fr>
2018-05-13 01:21:18 +0200

    [wip/pal-gripper] Fix naming mistakes in depend.mk

--- pal-gripper/depend.mk
+++ pal-gripper/depend.mk
@@ -15,14 +15,19 @@ PREFER.pal-gripper?=	robotpkg
 
 DEPEND_USE+=			pal-gripper
 
-DEPEND_ABI.pal-gripper?=	pal-gripper>=0.2.8
+DEPEND_ABI.pal-gripper?=	pal-gripper>=0.0.13
 DEPEND_DIR.pal-gripper?=	../../wip/pal-gripper
 
 SYSTEM_SEARCH.pal-gripper=\
-  'lib/pal_gripper_configuratio/home_gripper.py'		\
+  'lib/pal_gripper_controller_configuration/home_gripper.py'		\
   'share/pal_gripper/package.xml:/<version>/s/[^0-9.]//gp'	\
   'lib/pkgconfig/pal_gripper.pc:/Version/s/[^0-9.]//gp'  \
-  'share/pal_gripper/cmake/pal_gripperConfig.cmake'
+  'share/pal_gripper/cmake/pal_gripperConfig.cmake' \
+  'share/pal_gripper_gazebo/cmake/pal_gripper_gazeboConfig.cmake' \
+  'share/pal_gripper_controller_configuration/cmake/pal_gripper_controller_configurationConfig.cmake' \
+  'share/pal_gripper_controller_configuration_gazebo/cmake/pal_gripper_controller_configuration_gazeboConfig.cmake' \
+  'share/pal_gripper_description/cmake/pal_gripper_descriptionConfig.cmake' \
+  'share/pal_parallel_gripper_wrapper/cmake/pal_parallel_gripper_wrapperConfig.cmake'
 
 endif # PAL_GRIPPER_DEPEND_MK ----------------------------------------
 

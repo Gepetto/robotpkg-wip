b40bc29d71dce928d68ff5829f89363fa20b151f
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-05-25 14:05:49 +0200

    [wip/py-parametric-curves] add python bindings for parametric-curves

---
+++ py-parametric-curves/depend.mk
@@ -0,0 +1,26 @@
+# robotpkg depend.mk for:	wip/py-parametric-curves
+# Created:			Guilhem Saurel on Fri 25 May 2018
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+PY_PARAMETRIC_CURVES_DEPEND_MK:=	${PY_PARAMETRIC_CURVES_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		py-parametric-curves
+endif
+
+ifeq (+,$(PY_PARAMETRIC_CURVES_DEPEND_MK)) # -------------------------------------------
+
+PREFER.py-parametric-curves?=	robotpkg
+
+SYSTEM_SEARCH.py-parametric-curves=\
+	'${PYTHON_SYSLIBSEARCH}/parametric_curves/__init__.py'
+
+DEPEND_USE+=		py-parametric-curves
+
+DEPEND_ABI.py-parametric-curves?=	${PKGTAG.python-}parametric-curves>=1.0.0
+DEPEND_DIR.py-parametric-curves?=	../../wip/py-parametric-curves
+
+endif # PY_PARAMETRIC_CURVES_DEPEND_MK -------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

c172c3d7a9b3d811b9d65541f9ad9fbc9f311f37
Olivier Stasse <ostasse@laas.fr>
2018-06-20 23:12:34 +0200

    [wip/prf-gazebo-ros-pkgs] Increase precision of header dependencies.

--- prf-gazebo-ros-pkgs/depend.mk
+++ prf-gazebo-ros-pkgs/depend.mk
@@ -20,6 +20,8 @@ DEPEND_DIR.prf-gazebo-ros-pkgs?=	../../wip/prf-gazebo-ros-pkgs
 
 SYSTEM_SEARCH.prf-gazebo-ros-pkgs=\
   'include/gazebo_ros_control/gazebo_ros_control_plugin.h'	\
+  'include/gazebo_ros_control/default_robot_hw_sim.h'	\
+  'include/gazebo_ros_control/robot_hw_sim.h'	\
   'share/gazebo_ros_control/cmake/gazebo_ros_controlConfig.cmake' \
   'share/gazebo_ros_control/package.xml:/<version>/s/[^0-9.]//gp'	\
   'lib/pkgconfig/gazebo_ros_control.pc:/Version/s/[^0-9.]//gp'

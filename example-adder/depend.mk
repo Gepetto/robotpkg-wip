9d9a72bb82bdb9bedd29b97e50edff88cbd699fe
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-07-10 15:34:08 +0200

    Add example-adder & py-example-adder

---
+++ example-adder/depend.mk
@@ -0,0 +1,29 @@
+# robotpkg depend.mk for:	wip/example-adder
+# Created:			Guilhem Saurel on Tue, 10 Jul 2018
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+EXAMPLE_ADDER_DEPEND_MK:=	${EXAMPLE_ADDER_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		example-adder
+endif
+
+ifeq (+,$(EXAMPLE_ADDER_DEPEND_MK)) # ------------------------------------------
+
+PREFER.example-adder?=	robotpkg
+
+#TODO
+SYSTEM_SEARCH.example-adder=\
+  'include/example/adder/config.hh:/EXAMPLE_ADDER_VERSION/s/[^0-9.]//gp'	\
+  'lib/libexample-adder.so'							\
+  'lib/pkgconfig/example-adder.pc:/Version/s/[^0-9.]//gp'
+
+DEPEND_USE+=		example-adder
+
+DEPEND_ABI.example-adder?=	example-adder>=1.0.0
+DEPEND_DIR.example-adder?=	../../wip/example-adder
+
+endif # EXAMPLE_ADDER_DEPEND_MK ------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}
---
+++ py-example-adder/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	wip/py-example-adder
+# Created:			Guilhem Saurel on Tue, 10 Jul 2018
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+PY_EXAMPLE_ADDER_DEPEND_MK:=	${PY_EXAMPLE_ADDER_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		py-example-adder
+endif
+
+ifeq (+,$(EXAMPLE_ADDER_DEPEND_MK)) # ------------------------------------------
+
+PREFER.py-example-adder?=	robotpkg
+
+#TODO
+SYSTEM_SEARCH.py-example-adder=\
+  '${PYTHON_SYSLIBSEARCH}/example_adder/libexample-adder.so'							\
+  '${PYTHON_SYSLIBSEARCH}/example_adder/__init__.py'
+
+DEPEND_USE+=		py-example-adder
+
+DEPEND_ABI.py-example-adder?=	py-example-adder>=1.0.0
+DEPEND_DIR.py-example-adder?=	../../wip/py-example-adder
+
+endif # PY_EXAMPLE_ADDER_DEPEND_MK ------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

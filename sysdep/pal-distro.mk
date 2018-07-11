478a78d34a60cc0ef44418c2c3d263ac5dd5323a
Olivier Stasse <ostasse@laas.fr>
2018-07-11 05:19:43 +0200

    [wip/sysdep] Add macros and shell script to detect pal-distro pal-distro.mk: Fix the recursive inclusion protection.

---
+++ sysdep/pal-distro.mk
@@ -0,0 +1,15 @@
+# robotpkg wip/pal-erbium
+# Created:			Olivier Stasse on Mon 11 2018
+#
+ifndef _paldistro_mk
+  _paldistro_mk:=defined
+  _paldistro:=$(shell ${SHELL} ${ROBOTPKG_DIR}/wip/sysdep/pal-distro.sh)
+  export PALDISTRO:=  $(word 1,${_paldistro})
+
+#ifeq (yes,$(call exists,${ROBOTPKG_DIR}/wip/sysdep/platform/${PALDISTRO}.mk))
+#  include ${ROBOTPKG_DIR}/wip/sysdep/platform/${PALDISTRO}.mk
+#else
+#  $(error missing wip/sysdep/platform/${PALDISTRO}.mk)
+#endif
+
+endif # ERBIUM_ROS_FLAVOR

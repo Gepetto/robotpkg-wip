6fc6e83f6dc5686b95af1e58928ebf0c542bf98b
Ellon Paiva Mendes <ellon.mendes@laas.fr>
2018-08-01 15:30:56 +0200

    [wip/ros-swri-serial-util] Add version 2.3.0

---
+++ ros-swri-serial-util/depend.mk
@@ -0,0 +1,44 @@
+# robotpkg depend.mk for:	devel/ros-swri-serial-util
+# Created:			Ellon Paiva Mendes on Thu,  2 Aug 2018
+#
+
+DEPEND_DEPTH:=      ${DEPEND_DEPTH}+
+ROS_SWRI_SERIAL_UTIL_DEPEND_MK:= ${ROS_SWRI_SERIAL_UTIL_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=      ros-swri-serial-util
+endif
+
+ifeq (+,$(ROS_SWRI_SERIAL_UTIL_DEPEND_MK)) # ---------------------------------------
+
+include ../../meta-pkgs/ros-base/depend.common
+PREFER.ros-swri-serial-util?=    ${PREFER.ros-base}
+SYSTEM_PREFIX.ros-swri-serial-util?= ${SYSTEM_PREFIX.ros-base}
+
+DEPEND_USE+=      ros-swri-serial-util
+ROS_DEPEND_USE+=    ros-swri-serial-util
+
+DEPEND_ABI.ros-swri-serial-util?=  ros-swri-serial-util>=2.3.0
+DEPEND_DIR.ros-swri-serial-util= ../../wip/ros-swri-serial-util
+
+DEPEND_ABI.ros-swri-serial-util.groovy += ros-swri-serial-util>=2.3.0
+DEPEND_ABI.ros-swri-serial-util.hydro += ros-swri-serial-util>=2.3.0
+DEPEND_ABI.ros-swri-serial-util.indigo += ros-swri-serial-util>=2.3.0
+DEPEND_ABI.ros-swri-serial-util.jade += ros-swri-serial-util>=2.3.0
+DEPEND_ABI.ros-swri-serial-util.kinetic += ros-swri-serial-util>=2.3.0
+DEPEND_ABI.ros-swri-serial-util.lunar += ros-swri-serial-util>=2.3.0
+DEPEND_ABI.ros-swri-serial-util.melodic += ros-swri-serial-util>=2.3.0
+
+SYSTEM_SEARCH.ros-swri-serial-util=\
+  'share/swri_serial_util/package.xml' \
+  'share/swri_serial_util/cmake/swri_serial_utilConfig.cmake' \
+  'share/swri_serial_util/cmake/swri_serial_utilConfig-version.cmake' \
+  'include/swri_serial_util/serial_port.h' \
+  'lib/pkgconfig/swri_serial_util.pc:/Version/s/[^0-9.]//gp' \
+  'lib/libswri_serial_util.so'
+
+
+endif # ROS_SWRI_SERIAL_UTIL_DEPEND_MK ---------------------------------------------
+
+DEPEND_DEPTH:=    ${DEPEND_DEPTH:+=}
+

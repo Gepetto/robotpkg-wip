e43b38fa84cc57cc6d4fce12a636b023350a9f48
Ellon Paiva Mendes <ellon.mendes@laas.fr>
2018-08-14 13:30:45 +0200

    [wip/infuse-asn1-types] Updated to v0.2
    Changes since v0.1:
    
    * Installs a .pc file
    
    * Synchronizes .asn files with CDFF commit
      5a3e83c75932a622b85b3df0bc6b318576183bed. Main changes:
    
      * Map.asn rewritten
    
      * Most of sequences defined on Eigen.asn now have fixed sizes
    
      * New string type: StringN.
    
      * .asn files are now structured the same way as in CDFF

--- infuse-asn1-types/depend.mk
+++ infuse-asn1-types/depend.mk
@@ -15,26 +15,10 @@ PREFER.infuse-asn1-types?=    robotpkg
 
 DEPEND_USE+=      infuse-asn1-types
 
-DEPEND_ABI.infuse-asn1-types?=  infuse-asn1-types>=0.1
+DEPEND_ABI.infuse-asn1-types?=  infuse-asn1-types>=0.2
 DEPEND_DIR.infuse-asn1-types= ../../wip/infuse-asn1-types
 
 SYSTEM_SEARCH.infuse-asn1-types=\
-  'include/infuse_asn1_types/CorrespondenceMap2D.h' \
-  'include/infuse_asn1_types/CorrespondenceMap3D.h' \
-  'include/infuse_asn1_types/Covariance.h' \
-  'include/infuse_asn1_types/Eigen.h' \
-  'include/infuse_asn1_types/Geometry.h' \
-  'include/infuse_asn1_types/Image_InFuse.h' \
-  'include/infuse_asn1_types/Map.h' \
-  'include/infuse_asn1_types/Path.h' \
-  'include/infuse_asn1_types/Point.h' \
-  'include/infuse_asn1_types/Pointcloud.h' \
-  'include/infuse_asn1_types/Pose.h' \
-  'include/infuse_asn1_types/Time.h' \
-  'include/infuse_asn1_types/TransformWithCovariance.h' \
-  'include/infuse_asn1_types/TwistWithCovariance.h' \
-  'include/infuse_asn1_types/VisualPointFeatureVector2D.h' \
-  'include/infuse_asn1_types/VisualPointFeatureVector3D.h' \
   'include/infuse_asn1_types/asn1crt.h' \
   'include/infuse_asn1_types/taste-extended.h' \
   'include/infuse_asn1_types/taste-types.h' \
@@ -42,6 +26,7 @@ SYSTEM_SEARCH.infuse-asn1-types=\
   'lib/cmake/infuse_asn1_types/infuse_asn1_typesConfigVersion.cmake' \
   'lib/cmake/infuse_asn1_types/infuse_asn1_typesTargets-release.cmake' \
   'lib/cmake/infuse_asn1_types/infuse_asn1_typesTargets.cmake' \
+  'lib/pkgconfig/infuse_asn1_types.pc:/Version/s/[^0-9.]//gp' \
   'lib/libinfuse_asn1_types.so'
 
 endif # INFUSE_ASN1_TYPES_DEPEND_MK ---------------------------------------------

41e475167e686fe3acb22ba17d3ae5c6dc1ce41d
Quentin Labourey <quentin.labourey@laas.fr>
2018-08-17 17:18:20 +0200

    [wip/mk/sysdep] Added urdfdom dependency

---
+++ mk/sysdep/urdfdom.mk
@@ -0,0 +1,33 @@
+# robotpkg depend.mk for:   mono
+# Created:      Quentin Labourey on Fri, 17 August 2018
+
+DEPEND_DEPTH:=          ${DEPEND_DEPTH}+
+URDFDOM_DEPEND_MK:=     ${URDFDOM_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=        urdfdom
+endif
+
+ifeq (+,$(URDFDOM_DEPEND_MK))
+
+PREFER.urdfdom?=        system
+DEPEND_USE+=            urdfdom
+DEPEND_ABI.urdfdom=     urdfdom>=0.4
+SYSTEM_PKG.Ubuntu.urdfdom= liburdfdom-dev
+
+SYSTEM_SEARCH.urdfdom=\
+    'lib/pkgconfig/urdfdom_headers.pc' \
+	'lib/liburdfdom_model.so' \
+	'lib/liburdfdom_model_state.so' \
+	'lib/liburdfdom_sensor.so' \
+	'lib/pkgconfig/urdfdom.pc' \
+	'lib/pkgconfig/urdfdom_headers.pc' \
+	'share/urdfdom/cmake/urdfdom-config.cmake' \
+	'share/urdfdom_headers/cmake' \
+	'share/urdfdom_headers/cmake/urdfdom_headers-config-version.cmake' \
+	'share/urdfdom_headers/cmake/urdfdom_headers-config.cmake' \
+	'include/urdf_parser/urdf_parser.h'
+
+endif
+
+DEPEND_DEPTH:= ${DEPEND_DEPTH:+=}

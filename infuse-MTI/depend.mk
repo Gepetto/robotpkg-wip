fef2e476b319fcb749a0d5cf62116d6921513f4d
Pierre Narvor <pnarvor@laas.fr>
2018-08-22 11:13:23 +0200

    [wip/infuse-MTI] Fixed installation issue of infuse-MTI, made infuse_rmp400-genom3 depends on infuse-MTI

--- infuse-MTI/depend.mk
+++ infuse-MTI/depend.mk
@@ -3,21 +3,21 @@
 #
 
 DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-INFUSE_MTI_DEPEND_MK:=${INFUSE_MTI_DEPEND_MK}+
+infuse-MTI_DEPEND_MK:=${infuse-MTI_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=		infuse-MTI
 endif
 
-ifeq (+,$(INFUSE_MTI_DEPEND_MK))
-PREFER.INFUSE_MTI?=	robotpkg
+ifeq (+,$(infuse-MTI_DEPEND_MK))
+PREFER.infuse-MTI?=	robotpkg
 
 DEPEND_USE+=		infuse-MTI
 
-DEPEND_ABI.INFUSE_MTI?=	infuse-MTI>=0.7
-DEPEND_DIR.INFUSE_MTI?=	../../wip/infuse-MTI
+DEPEND_ABI.infuse-MTI?=	infuse-MTI>=0.7
+DEPEND_DIR.infuse-MTI?=	../../wip/infuse-MTI
 
-SYSTEM_SEARCH.INFUSE_MTI=\
+SYSTEM_SEARCH.infuse-MTI=\
 	include/MTI-clients/MTI.h \
 	lib/pkgconfig/MTI-clients.pc
 endif

cdc58c4148fa19118671639189d6ba7131e91768
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-08-23 12:48:05 +0200

    [wip/sot-torque-control] Bump to v1.1.0
    Changes since the last version:
    - Remove shell
    - Add Gitlab-CI, update submodule, fix cmake, add BUILD_PYTHON_INTERFACE option
    - Remove inclusion to allow c++-11 less compiling.

--- sot-torque-control/depend.mk
+++ sot-torque-control/depend.mk
@@ -1,4 +1,4 @@
-# robotpkg depend.mk for:	wip/sot-core
+# robotpkg depend.mk for:	wip/sot-torque-control
 # Created:			Rohan Budhiraja on Thu, 17 Dec 2015
 #
 
@@ -20,7 +20,7 @@ SYSTEM_SEARCH.sot-torque-control=\
 
 DEPEND_USE+=		sot-torque-control
 
-DEPEND_ABI.sot-torque-control?=	sot-torque-control>=3.0.0
+DEPEND_ABI.sot-torque-control?=	sot-torque-control>=1.0.0
 DEPEND_DIR.sot-torque-control?=	../../wip/sot-torque-control
 
 endif # SOT_CORE_DEPEND_MK -------------------------------------------------

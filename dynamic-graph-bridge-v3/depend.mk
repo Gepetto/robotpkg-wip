1a5d6cb8638416eedeed7310198faf759e6d8b70
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-08-23 13:02:39 +0200

    [wip/dynamic-graph-bridge-v3] Bump to v3.1.0
    Changes since the last version:
    - [CMake] add BUILD_PYTHON_INTERFACE option
    - Add entity RosQueuedSubscribe
    
    Changes in the packaging:
    - split the package for python

--- dynamic-graph-bridge-v3/depend.mk
+++ dynamic-graph-bridge-v3/depend.mk
@@ -1,4 +1,4 @@
-# robotpkg Makefile for:	wip/dynamic-graph-bridge
+# robotpkg Makefile for:	wip/dynamic-graph-bridge-v3
 #
 
 DEPEND_DEPTH:=			${DEPEND_DEPTH}+
@@ -18,14 +18,10 @@ DEPEND_ABI.ros+=		ros>=groovy
 DEPEND_ABI.dynamic-graph-bridge-v3?=		dynamic-graph-bridge-v3>=3.0.0
 DEPEND_DIR.dynamic-graph-bridge-v3?=		../../wip/dynamic-graph-bridge-v3
 
-DEPEND_ABI.dynamic-graph-bridge-v3.groovy?=	dynamic-graph-bridge-v3>=3.0.0
-DEPEND_ABI.dynamic-graph-bridge-v3.hydro?=	dynamic-graph-bridge-v3>=3.0.0
-
 SYSTEM_SEARCH.dynamic-graph-bridge-v3=\
   'include/dynamic_graph_bridge/config.hh'				\
   'lib/pkgconfig/dynamic_graph_bridge.pc:/Version/s/[^0-9.]//gp'	\
   'share/dynamic_graph_bridge/robot_pose_publisher'			\
-  '${PYTHON_SITELIB}/dynamic_graph/ros/ros.py'	\
   'share/dynamic_graph_bridge/cmake/dynamic_graph_bridgeConfig.cmake'
 
 

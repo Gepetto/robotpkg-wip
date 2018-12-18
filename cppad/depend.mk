0f1dc49a31cc6f7db17b21d2cbda005d139c28bd
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-12-18 14:11:16 +0100

    [wip/cppad] prefer system pkg on Debian, Fedora & recent Ubuntu

--- cppad/depend.mk
+++ cppad/depend.mk
@@ -11,15 +11,31 @@ endif
 
 ifeq (+,$(CPPAD_DEPEND_MK)) # ------------------------------------------
 
+include ../../mk/robotpkg.prefs.mk # for OPSYS
+ifeq (Ubuntu,${OPSYS})
+  ifneq (,$(filter 12.04 14.04,${OS_VERSION}))
+    PREFER.cppad?=	robotpkg
+  else
+    PREFER.cppad?=	system
+  endif
+else ifeq (Fedora,${OPSYS})
+  PREFER.cppad?=	system
+else ifeq (Debian,${OPSYS})
+  PREFER.cppad?=	system
+endif
 PREFER.cppad?=	robotpkg
 
 SYSTEM_SEARCH.cppad=\
   'include/cppad/configure.hpp:'	\
   'share/pkgconfig/cppad.pc:/Version  /s/[^0-9.]//gp'
 
+SYSTEM_PKG.Debian.cppad=cppad
+SYSTEM_PKG.Ubuntu.cppad=cppad
+SYSTEM_PKG.Fedora.cppad=cppad-devel
+
 DEPEND_USE+=		cppad
 
-DEPEND_ABI.cppad?=	CppAD>=20180000.0
+DEPEND_ABI.cppad?=	CppAD>=2014.00.00.3
 DEPEND_DIR.cppad?=	../../wip/cppad
 
 endif # CPPAD_DEPEND_MK ------------------------------------------------

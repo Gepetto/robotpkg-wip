6a195ef084e0fc70d42f0be0cc977480a33a5c04
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-12-19 18:35:08 +0100

    [wip/universal-robot] Initial Import

---
+++ universal-robot/depend.mk
@@ -0,0 +1,27 @@
+# robotpkg depend.mk for:	wip/universal-robot
+# Created:			Guilhem Saurel on Fri, 19 Oct 2018
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+UNIVERSAL_ROBOT_DEPEND_MK:=	${UNIVERSAL_ROBOT_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		universal-robot
+endif
+
+ifeq (+,$(UNIVERSAL_ROBOT_DEPEND_MK)) # --------------------------------------
+
+PREFER.universal-robot?=	robotpkg
+
+DEPEND_USE+=		universal-robot
+
+DEPEND_ABI.universal-robot?=	universal-robot>=1.2.1
+DEPEND_DIR.universal-robot?=	../../wip/universal-robot
+
+SYSTEM_SEARCH.universal-robot=\
+	include/hpp/romeo/config.hh	\
+	'lib/pkgconfig/universal-robot.pc:/Version/s/[^0-9.]//gp'
+
+endif # UNIVERSAL_ROBOT_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

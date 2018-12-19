f2e21ae38f15007e7c677545ae52c7878837373a
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-12-19 18:21:02 +0100

    [wip/hpp-romeo] Initial Import

---
+++ hpp-romeo/depend.mk
@@ -0,0 +1,27 @@
+# robotpkg depend.mk for:	wip/hpp-romeo
+# Created:			Guilhem Saurel on Fri, 19 Oct 2018
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+HPPROMEO_DEPEND_MK:=	${HPPROMEO_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		hpp-romeo
+endif
+
+ifeq (+,$(HPPROMEO_DEPEND_MK)) # --------------------------------------
+
+PREFER.hpp-romeo?=	robotpkg
+
+DEPEND_USE+=		hpp-romeo
+
+DEPEND_ABI.hpp-romeo?=	hpp-romeo>=4.2.0
+DEPEND_DIR.hpp-romeo?=	../../wip/hpp-romeo
+
+SYSTEM_SEARCH.hpp-romeo=\
+	include/hpp/romeo/config.hh	\
+	'lib/pkgconfig/hpp_romeo.pc:/Version/s/[^0-9.]//gp'
+
+endif # HPPROMEO_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

066f0daf119c3c410e1c68b57a9ecbdbc0831fe7
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-01-04 17:43:01 +0100

    [wip/hpp-affordance] Initial import

---
+++ hpp-affordance/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	wip/hpp-affordance
+# Created:			Guilhem Saurel on Fri, 4 Jan 2019
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+HPP_AFFORDANCE_DEPEND_MK:=	${HPP_AFFORDANCE_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		hpp-affordance
+endif
+
+ifeq (+,$(HPP_AFFORDANCE_DEPEND_MK)) # --------------------------------------
+
+PREFER.hpp-affordance?=	robotpkg
+
+DEPEND_USE+=		hpp-affordance
+
+DEPEND_ABI.hpp-affordance?=	hpp-affordance>=4.2.0
+DEPEND_DIR.hpp-affordance?=	../../wip/hpp-affordance
+
+SYSTEM_SEARCH.hpp-affordance=\
+	include/hpp/affordance/config.hh	\
+	lib/libhpp-affordance.so	\
+	'lib/pkgconfig/hpp-affordance.pc:/Version/s/[^0-9.]//gp'
+
+endif # HPP_AFFORDANCE_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

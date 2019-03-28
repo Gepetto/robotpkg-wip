bc5c62661f07098412c48516749bf678c726d0f3
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-03-28 13:36:54 +0100

    [wip/py-multicontact-api] fix SYSTEM_SEARCH

--- py-multicontact-api/depend.mk
+++ py-multicontact-api/depend.mk
@@ -14,10 +14,9 @@ ifeq (+,$(PY_MULTICONTACT_API_DEPEND_MK)) # ------------------------------------
 PREFER.py-multicontact-api?=	robotpkg
 
 SYSTEM_SEARCH.py-multicontact-api=\
-  'include/multicontact-api/config.hh:/MULTICONTACT_API_VERSION/s/[^0-9.]//gp'	\
-  'lib/pkgconfig/multicontact-api.pc:/Version/s/[^0-9.]//gp'
+	'${PYTHON_SYSLIBSEARCH}/multicontact_api/libmulticontact_api.so'
 
-DEPEND_USE+=				py-multicontact-api
+DEPEND_USE+=					py-multicontact-api
 
 DEPEND_ABI.py-multicontact-api?=		py-multicontact-api>=1.0.0
 DEPEND_DIR.py-multicontact-api?=		../../wip/py-multicontact-api

d2628575b01c18149b1d20c0eaea8f1f4577ac72
Olivier Stasse <ostasse@laas.fr>
2019-04-04 07:38:27 +0200

    [wip/arucos] Fix depend.mk

--- aruco-ros/depend.mk
+++ aruco-ros/depend.mk
@@ -15,7 +15,7 @@ PREFER.aruco-ros?=	robotpkg
 
 DEPEND_USE+=			aruco-ros
 
-DEPEND_ABI.aruco-ros?=	aruco-ros>=0.2.8
+DEPEND_ABI.aruco-ros?=	aruco-ros>=0.2.3
 DEPEND_DIR.aruco-ros?=	../../wip/aruco-ros
 
 SYSTEM_SEARCH.aruco-ros=\
@@ -23,6 +23,8 @@ SYSTEM_SEARCH.aruco-ros=\
   'lib/libaruco_ros_utils.so'		\
   'share/aruco/package.xml:/<version>/s/[^0-9.]//gp'	\
   'lib/pkgconfig/aruco.pc:/Version/s/[^0-9.]//gp'  \
+  'lib/pkgconfig/aruco_ros.pc:/Version/s/[^0-9.]//gp'  \
+  'share/aruco_ros/cmake/aruco_rosConfig.cmake' \
   'share/aruco/cmake/arucoConfig.cmake'
 
 endif # ARUCO_ROS_DEPEND_MK ----------------------------------------

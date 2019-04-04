547e08a64cc6f9c0788fa085b28b5d0271801626
Olivier Stasse <ostasse@laas.fr>
2019-04-04 10:58:19 +0200

    [wip/prf-teleop-tools] Initial commit

---
+++ prf-teleop-tools/depend.mk
@@ -0,0 +1,29 @@
+# robotpkg depend.mk for:	wip/teleop-tools
+# Created:			Olivier Stasse on Wed, 29 Mar 2017
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+TELEOP_TOOLS_DEPEND_MK:=	${TELEOP_TOOLS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			prf-teleop-tools
+endif
+
+ifeq (+,$(TELEOP_TOOLS_DEPEND_MK)) # ----------------------------------
+
+PREFER.teleop-tools?=	robotpkg
+
+DEPEND_USE+=			prf-teleop-tools
+
+DEPEND_ABI.prf-teleop-tools?=	prf-teleop-tools>=0.3.1
+DEPEND_DIR.prf-teleop-tools?=	../../wip/prf-teleop-tools
+
+SYSTEM_SEARCH.prf-teleop-tools=\
+  'include/joy_teleop/incrementer_server.h'		\
+  'share/joy_teleop/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/joy_teleop.pc:/Version/s/[^0-9.]//gp'  \
+  'share/joy_teleop/cmake/joy_teleopConfig.cmake'
+
+endif # TELEOP_TOOLS_DEPEND_MK ----------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

18e7d660ccb1e5d84ee27f74582a141f1b9dbc2e
Olivier Stasse <ostasse@laas.fr>
2019-04-12 16:38:13 +0200

    [wip/pmb2-navigation] Fix depend.mk Fix wrong files to be look for and wrong variable.

--- pmb2-navigation/depend.mk
+++ pmb2-navigation/depend.mk
@@ -20,8 +20,11 @@ DEPEND_DIR.pmb2-navigation?=	../../wip/pmb2-navigation
 
 SYSTEM_SEARCH.pmb2-navigation=\
   'share/pmb2_navigation/package.xml:/<version>/s/[^0-9.]//gp'	\
-  'lib/pkgconfig/pmb2_navigation.pc:/Version/s/[^0-9.]//gp'  \
-  'share/pmb2_navigation/cmake/pmb2_navigationConfig.cmake'
+  'lib/pkgconfig/pmb2_2dnav.pc:/Version/s/[^0-9.]//gp'  \
+  'share/pmb2_2dnav/cmake/pmb2_2dnavConfig.cmake' \
+  'share/pmb2_laser_sensors/cmake/pmb2_laser_sensorsConfig.cmake' \
+  'share/pmb2_maps/cmake/pmb2_mapsConfig.cmake' \
+  'share/pmb2_rgbd_sensors/cmake/pmb2_rgbd_sensorsConfig.cmake'
 
 endif # PMB2_NAVIGATION_DEPEND_MK ----------------------------------------
 

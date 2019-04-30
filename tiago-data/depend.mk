5ed963c73b08b6872630047af3cf770a891d0862
Olivier Stasse <ostasse@laas.fr>
2019-04-30 19:10:57 +0200

    [wip/tiago-data] Release 0.0.1 package which export URDF file of Tiago without xacro.

---
+++ tiago-data/depend.mk
@@ -0,0 +1,27 @@
+# robotpkg depend.mk for:	wip/tiago-data
+# Created:			Olivier Stasse on Thu 30th April 2019
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+TIAGO_DATA_DEPEND_MK:=	${TIAGO_DATA_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		tiago-data
+endif
+
+ifeq (+,$(TIAGO_DATA_DEPEND_MK)) # -------------------------------------------
+
+PREFER.tiago-data?=	robotpkg
+
+SYSTEM_SEARCH.tiago-data=\
+	'share/tiago_data/urdf/tiago_reduced.urdf'				\
+	'lib/pkgconfig/tiago_data.pc:/Version/s/[^0-9.]//gp'
+
+DEPEND_USE+=		tiago-data
+
+DEPEND_ABI.tiago-data?=	tiago-data>=0.0.1
+DEPEND_DIR.tiago-data?=	../../wip/tiago-data
+
+endif # TIAGO_DATA_DEPEND_MK -------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

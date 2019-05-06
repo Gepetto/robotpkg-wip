7101372588a5a3baaf15bacb9ec25e8955ecf328
Olivier Stasse <ostasse@laas.fr>
2019-05-06 10:06:25 +0200

    [wip/tiago-simulation] Fix depend.mk

--- tiago-simulation/depend.mk
+++ tiago-simulation/depend.mk
@@ -20,8 +20,14 @@ DEPEND_DIR.tiago-simulation?=	../../wip/tiago-simulation
 
 SYSTEM_SEARCH.tiago-simulation=\
   'share/tiago_simulation/package.xml:/<version>/s/[^0-9.]//gp'	\
-  'lib/pkgconfig/tiago_simulation.pc:/Version/s/[^0-9.]//gp'  \
-  'share/tiago_simulation/cmake/tiago_simulationConfig.cmake'
+  'lib/pkgconfig/tiago_2dnav_gazebo.pc:/Version/s/[^0-9.]//gp'  \
+  'lib/pkgconfig/tiago_controller_configuration_gazebo.pc:/Version/s/[^0-9.]//gp'  \
+  'lib/pkgconfig/tiago_gazebo.pc:/Version/s/[^0-9.]//gp'  \
+  'lib/pkgconfig/tiago_multi.pc:/Version/s/[^0-9.]//gp'  \
+  'share/tiago_2dnav_gazebo/cmake/tiago_2dnav_gazeboConfig.cmake' \
+  'share/tiago_controller_configuration_gazebo/cmake/tiago_controller_configuration_gazeboConfig.cmake' \
+  'share/tiago_gazebo/cmake/tiago_gazeboConfig.cmake' \
+  'share/tiago_multi/cmake/tiago_multiConfig.cmake'
 
 endif # TIAGO_SIMULATION_DEPEND_MK ----------------------------------------
 

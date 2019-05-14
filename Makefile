4e1028cba95849e3193ed60fcb374f823b2ec452
Olivier Stasse <ostasse@laas.fr>
2019-05-14 17:27:14 +0200

    [wip/sot-talos-balance] Add stabilization for TALOS (Pyrene) v1.3.1 Tested on the real robot for a 10 cm/s walk

--- Makefile
+++ Makefile
@@ -142,6 +142,7 @@ SUBDIR+=	sot-hrp2
 SUBDIR+=	sot-hrp2-v3
 SUBDIR+=	sot-pattern-generator-v3
 SUBDIR+=	sot-talos
+SUBDIR+=	sot-talos-balance
 SUBDIR+=	sot-tiago
 SUBDIR+=	sot-tools-v3
 SUBDIR+=	sot-torque-control
---
+++ sot-talos-balance/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	wip/sot-talos-balance
+# Created:			Olivier Stasse on Tue, 14 May 2019
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+SOT_TALOS_BALANCE_DEPEND_MK:=	${SOT_TALOS_BALANCE_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		sot-core-v3
+endif
+
+ifeq (+,$(SOT_TALOS_BALANCE_DEPEND_MK)) # -------------------------------------------
+
+PREFER.sot-talos-balance?=	robotpkg
+
+SYSTEM_SEARCH.sot-talos-balance=\
+	include/sot/talos_balance/dcm-com-controller.hh      	\
+	lib/libsot-talos-balance.so					\
+	'lib/pkgconfig/sot-talos-balance.pc:/Version/s/[^0-9.]//gp'
+
+DEPEND_USE+=		sot-talos-balance
+
+DEPEND_ABI.sot-talos-balance?=	sot-talos-balance>=1.3.1
+DEPEND_DIR.sot-talos-balance?=	../../wip/sot-talos-balance
+
+endif # SOT_CORE_DEPEND_MK -------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

a53bce721fcf80bf5290cbdf2e893adafdafc703
Olivier Stasse <ostasse@laas.fr>
2019-05-29 12:24:48 +0200

    [wip/py-sot-talos-balance] Add python bindings package for sot-talos-balance.

---
+++ py-sot-talos-balance/depend.mk
@@ -0,0 +1,27 @@
+# robotpkg depend.mk for:	wip/py-sot-talos-balance
+# Created:			Olivier Stasse on Tue, 14 May 2019
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+PY_SOT_TALOS_BALANCE_DEPEND_MK:=	${PY_SOT_TALOS_BALANCE_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		py-sot-talos-balance
+endif
+
+ifeq (+,$(PY_SOT_TALOS_BALANCE_DEPEND_MK)) # -------------------------------------------
+
+PREFER.py-sot-talos-balance?=	robotpkg
+
+SYSTEM_SEARCH.py-sot-talos-balance=\
+  '${PYTHON_SYSLIBSEARCH}/dynamic_graph/sot/talos_balance/inverse_dynamics_balance_controller/wrap.so' \
+  '${PYTHON_SYSLIBSEARCH}/dynamic_graph/sot/talos_balance/__init__.py'
+
+DEPEND_USE+=		py-sot-talos-balance
+
+DEPEND_ABI.py-sot-talos-balance?=	${PKGTAG.python-}sot-talos-balance>=1.3.0
+DEPEND_DIR.py-sot-talos-balance?=	../../wip/py-sot-talos-balance
+
+endif # SOT_CORE_DEPEND_MK -------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

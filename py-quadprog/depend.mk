63801aafb401636e3069fda611b23c5314ed9f7c
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-07-05 15:55:54 +0200

    [wip/py-quadprog] fix SYSTEM_SEARCH for python 3

--- py-quadprog/depend.mk
+++ py-quadprog/depend.mk
@@ -21,7 +21,7 @@ DEPEND_ABI.py-quadprog?=	${PKGTAG.python-}quadprog>=0.1.6
 DEPEND_DIR.py-quadprog?=	../../wip/py-quadprog
 
 SYSTEM_SEARCH.py-quadprog=\
-  '${PYTHON_SITELIB}/quadprog${PYTHON_SOABI}.so'
+  '${PYTHON_SITELIB}/quadprog${PYTHON_EXT_SUFFIX}'
 
 endif # PY_QUADPROG_DEPEND_MK --------------------------------------------------
 

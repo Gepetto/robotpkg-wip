e305aadeeaa46ca9eb62d051ea4eeb64f8233d41
Anthony Mallet <anthony.mallet@laas.fr>
2019-07-16 18:56:16 +0200

    [wip/py-quadprog] Don't reference PYTHON_EXT_SUFFIX in SYSTEM_SEARCH
    PYTHON_EXT_SUFFIX cannot be used before dependency resolution.
    
    Fixes #212

--- py-quadprog/depend.mk
+++ py-quadprog/depend.mk
@@ -21,7 +21,7 @@ DEPEND_ABI.py-quadprog?=	${PKGTAG.python-}quadprog>=0.1.6
 DEPEND_DIR.py-quadprog?=	../../wip/py-quadprog
 
 SYSTEM_SEARCH.py-quadprog=\
-  '${PYTHON_SITELIB}/quadprog${PYTHON_EXT_SUFFIX}'
+  '${PYTHON_SITELIB}/quadprog.{*.,}so'
 
 endif # PY_QUADPROG_DEPEND_MK --------------------------------------------------
 

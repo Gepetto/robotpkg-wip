add216163b69dc3d9e292f8155040001f9698b83
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-09-26 18:36:29 +0200

    [wip/hpp-affordance-corba] Update to v4.7.0
    Changes since v4.5.0:
    - Use plugin mechanism of hpp-corbaserver.
    - Fix compatibility with Python 3.
    - include pinocchio before boost
    
    Packaging changes:
    - removed patch aa, applied upstream

--- hpp-affordance-corba/depend.mk
+++ hpp-affordance-corba/depend.mk
@@ -11,16 +11,18 @@ endif
 
 ifeq (+,$(HPP_AFFORDANCE_CORBA_DEPEND_MK)) # --------------------------------------
 
+include ../../mk/sysdep/python.mk
+
 PREFER.hpp-affordance-corba?=	robotpkg
 
 DEPEND_USE+=		hpp-affordance-corba
 
-DEPEND_ABI.hpp-affordance-corba?=	hpp-affordance-corba>=4.3.0
+DEPEND_ABI.hpp-affordance-corba?=	${PKGTAG.python-}hpp-affordance-corba>=4.7.0
 DEPEND_DIR.hpp-affordance-corba?=	../../wip/hpp-affordance-corba
 
 SYSTEM_SEARCH.hpp-affordance-corba=\
-	include/hpp/corbaserver/affordance/config.hh	\
-	lib/libhpp-affordance-corba.so	\
+	include/hpp/corbaserver/affordance/config.hh			\
+	lib/hppPlugins/affordance-corba.so				\
 	'lib/pkgconfig/hpp-affordance-corba.pc:/Version/s/[^0-9.]//gp'
 
 endif # HPP_AFFORDANCE_CORBA_DEPEND_MK --------------------------------------------

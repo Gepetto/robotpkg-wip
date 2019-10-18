3a719f082554698c6cb586dadc73183ab8d053ee
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-10-18 13:38:58 +0200

    [wip/py-solo-pybullet] Initial Release

--- Makefile
+++ Makefile
@@ -105,6 +105,7 @@ SUBDIR+=	py-qpoases
 SUBDIR+=	py-qt-gepetto-pythonqt
 SUBDIR+=	py-quadprog
 SUBDIR+=	py-skeleton-model
+SUBDIR+=	py-solo-pybullet
 SUBDIR+=	py-sot-application-v3
 SUBDIR+=	py-sot-core-v3
 SUBDIR+=	py-sot-dynamic-pinocchio-v3
---
+++ py-solo-pybullet/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	wip/py37-solo-pybullet
+# Created:			Guilhem Saurel on Thu, 17 Oct 2019
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+PY_SOLO_PYBULLET_DEPEND_MK:=	${PY_SOLO_PYBULLET_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		py-solo-pybullet
+endif
+
+ifeq (+,$(PY_SOLO_PYBULLET_DEPEND_MK)) # --------------------------------------
+
+PREFER.py-solo-pybullet?=	robotpkg
+
+DEPEND_USE+=		py-solo-pybullet
+
+DEPEND_ABI.py-solo-pybullet?=	${PKGTAG.python-}solo-pybullet>=0.1.0
+DEPEND_DIR.py-solo-pybullet?=	../../wip/py-py-solo-pybullet
+
+SYSTEM_SEARCH.py-solo-pybullet=\
+	'${PYTHON_SYSLIBSEARCH}/solo_pybullet/__init__.py'
+
+include ../../mk/sysdep/python.mk
+
+endif # PY_SOLO_PYBULLET_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

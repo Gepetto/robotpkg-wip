8a5ce6fe7234f0e48795fbc4c4d8ce9b7954f5fb
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-10-23 12:10:08 +0200

    [wip/py-solo-pybullet] typo

--- py-solo-pybullet/depend.mk
+++ py-solo-pybullet/depend.mk
@@ -6,22 +6,22 @@ DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 PY_SOLO_PYBULLET_DEPEND_MK:=	${PY_SOLO_PYBULLET_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		py-solo-pybullet
+DEPEND_PKG+=			py-solo-pybullet
 endif
 
 ifeq (+,$(PY_SOLO_PYBULLET_DEPEND_MK)) # --------------------------------------
 
+include ../../mk/sysdep/python.mk
+
 PREFER.py-solo-pybullet?=	robotpkg
 
-DEPEND_USE+=		py-solo-pybullet
+DEPEND_USE+=			py-solo-pybullet
 
 DEPEND_ABI.py-solo-pybullet?=	${PKGTAG.python-}solo-pybullet>=0.1.0
-DEPEND_DIR.py-solo-pybullet?=	../../wip/py-py-solo-pybullet
+DEPEND_DIR.py-solo-pybullet?=	../../wip/py-solo-pybullet
 
 SYSTEM_SEARCH.py-solo-pybullet=\
 	'${PYTHON_SYSLIBSEARCH}/solo_pybullet/__init__.py'
-
-include ../../mk/sysdep/python.mk
 
 endif # PY_SOLO_PYBULLET_DEPEND_MK --------------------------------------------
 

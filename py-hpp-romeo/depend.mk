45d5fa2a8d7257029c8aee43241353f3b9c41304
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-11-05 14:27:32 +0100

    [wip/{ → py-}hpp-romeo] add PYTHON_NOTAG_CONFLICT

---
+++ py-hpp-romeo/depend.mk
@@ -0,0 +1,29 @@
+# robotpkg depend.mk for:	wip/py-hpp-romeo
+# Created:			Guilhem Saurel on Fri, 19 Oct 2018
+#
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+PY_HPP_ROMEO_DEPEND_MK:=	${PY_HPP_ROMEO_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=		py-hpp-romeo
+endif
+
+ifeq (+,$(PY_HPP_ROMEO_DEPEND_MK)) # --------------------------------------
+
+include ../../mk/sysdep/python.mk
+
+PREFER.py-hpp-romeo?=	robotpkg
+
+DEPEND_USE+=		py-hpp-romeo
+
+DEPEND_ABI.py-hpp-romeo?=	${PKGTAG.python-}hpp-romeo>=4.7.0
+DEPEND_DIR.py-hpp-romeo?=	../../wip/py-hpp-romeo
+
+SYSTEM_SEARCH.py-hpp-romeo=\
+	include/hpp/romeo/config.hh	\
+	'lib/pkgconfig/hpp_romeo.pc:/Version/s/[^0-9.]//gp'
+
+endif # PY_HPP_ROMEO_DEPEND_MK --------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

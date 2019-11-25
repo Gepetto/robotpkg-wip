7da6fce8ca8c2b36d603e802f496938fdaf50b13
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-11-25 23:56:15 +0100

    [wip/{py-,}example-robot-data] Update to v3.1.0
    Changes since v3.0.0:
    - add path.hpp
    - install package.xml
    - update & clean CMake

--- example-robot-data/depend.mk
+++ example-robot-data/depend.mk
@@ -15,12 +15,13 @@ PREFER.example-robot-data?=	robotpkg
 
 #TODO
 SYSTEM_SEARCH.example-robot-data=\
-  'include/example-robot-data/config.hh:/EXAMPLE_ROBOT_DATA_VERSION/s/[^0-9.]//gp'	\
-  'lib/pkgconfig/example-robot-data.pc:/Version/s/[^0-9.]//gp'
+  'include/example-robot-data/config.hh:/EXAMPLE_ROBOT_DATA_VERSION /s/[^0-9.]//gp'	\
+  'lib/pkgconfig/example-robot-data.pc:/Version/s/[^0-9.]//gp'				\
+  'share/example-robot-data/package.xml:/<version>/s/[^0-9.]//gp'
 
 DEPEND_USE+=		example-robot-data
 
-DEPEND_ABI.example-robot-data?=	example-robot-data>=0.1.0
+DEPEND_ABI.example-robot-data?=	example-robot-data>=3.0.0
 DEPEND_DIR.example-robot-data?=	../../wip/example-robot-data
 
 endif # EXAMPLE_ROBOT_DATA_DEPEND_MK ------------------------------------------------

27c3a263f62809b15e46ab42cc58a00562d00c02
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-01-10 19:11:00 +0100

    [wip/dynamic-graph-v3] Update to v3.8.0
    Changes since v3.7.3:
    - package.xml
    - Modern CMake

--- dynamic-graph-v3/depend.mk
+++ dynamic-graph-v3/depend.mk
@@ -14,9 +14,11 @@ ifeq (+,$(HPP_GIK_DEPEND_MK)) # --------------------------------------
 PREFER.dynamic-graph-v3?=	robotpkg
 
 SYSTEM_SEARCH.dynamic-graph-v3=\
-	include/dynamic-graph/entity.h	\
-	lib/libdynamic-graph.so		\
-	lib/pkgconfig/dynamic-graph.pc
+  'include/dynamic-graph/config.hh:/DYNAMIC_GRAPH_VERSION /s/[^0-9.]//gp'			\
+  'lib/cmake/dynamic-graph/dynamic-graphConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/dynamic-graph.pc:/Version/s/[^0-9.]//gp'					\
+  'lib/libdynamic-graph.so'									\
+  'share/dynamic-graph/package.xml:/<version>/s/[^0-9.]//gp'
 
 DEPEND_USE+=		dynamic-graph-v3
 

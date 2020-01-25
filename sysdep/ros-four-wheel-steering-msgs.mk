7e3041a515445419041b5054fa5e88077e792aeb
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-01-25 12:59:33 +0100

    [wip/sysdep/ros-four-wheel-steering-msgs] Update CMAKE_PREFIX_PATH
    to fix build of wip/prf-ros-controllers on 16.04 & python3

--- sysdep/ros-four-wheel-steering-msgs.mk
+++ sysdep/ros-four-wheel-steering-msgs.mk
@@ -5,7 +5,7 @@ DEPEND_DEPTH:=		${DEPEND_DEPTH}+
 FOUR_WHEEL_STEERING_MSGS_DEPEND_MK:=	${FOUR_WHEEL_STEERING_MSGS_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		ros-four-wheel-steering-msgs
+DEPEND_PKG+=					ros-four-wheel-steering-msgs
 endif
 
 ifeq (+,$(FOUR_WHEEL_STEERING_MSGS_DEPEND_MK)) # ---------------------------------------------
@@ -13,14 +13,15 @@ ifeq (+,$(FOUR_WHEEL_STEERING_MSGS_DEPEND_MK)) # -------------------------------
 include ../../meta-pkgs/ros-base/depend.common
 
 PREFER.ros-four-wheel-steering-msgs?=		system
-SYSTEM_PREFIX.ros-four-wheel-steering-msgs?=    ${SYSTEM_PREFIX.ros-base}
+SYSTEM_PREFIX.ros-four-wheel-steering-msgs?=	${SYSTEM_PREFIX.ros-base}
+CMAKE_PREFIX_PATH+=				${SYSTEM_PREFIX.ros-four-wheel-steering-msgs}
 
-DEPEND_USE+=		ros-four-wheel-steering-msgs
-ROS_DEPEND_USE+=		ros-four-wheel-steering-msgs
+DEPEND_USE+=					ros-four-wheel-steering-msgs
+ROS_DEPEND_USE+=				ros-four-wheel-steering-msgs
 
 DEPEND_ABI.ros-four-wheel-steering-msgs?=	ros-four-wheel-steering-msgs>=1
 
-DEPEND_METHOD.ros-four-wheel-steering-msgs+=build
+DEPEND_METHOD.ros-four-wheel-steering-msgs+=	build
 
 SYSTEM_SEARCH.ros-four-wheel-steering-msgs+=	'share/four_wheel_steering_msgs/package.xml:/<version>/s/[^.0-9]//gp'
 

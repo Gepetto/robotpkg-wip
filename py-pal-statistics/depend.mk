3e6fa48ed0d2987a7287728a51b3b3c3fda06b83
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-03-23 21:09:16 +0100

    [wip/{ → py-}pal-statistics] add patch-ab for python 3
    While here, use PKGTAG.python- prefix & fix PLIST
    
    The renaming affects the following packages:
    
    - wip/pal-hardware-gazebo-melodic
    - wip/pal-hardware-gazebo

---
+++ py-pal-statistics/depend.mk
@@ -0,0 +1,30 @@
+# robotpkg depend.mk for:	wip/py-pal-statistics
+# Created:			Guilhem Saurel on Mon, 23 Mar 2020
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+PY_PAL_STATISTICS_DEPEND_MK:=	${PY_PAL_STATISTICS_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			py-pal-statistics
+endif
+
+ifeq (+,$(PY_PAL_STATISTICS_DEPEND_MK)) # ---------------------------------
+
+PREFER.py-pal-statistics?=	robotpkg
+
+DEPEND_USE+=			py-pal-statistics
+
+DEPEND_ABI.py-pal-statistics?=	py-pal-statistics>=1.1.0
+DEPEND_DIR.py-pal-statistics?=	../../wip/py-pal-statistics
+
+SYSTEM_SEARCH.py-pal-statistics=\
+  'include/pal_statistics/pal_statistics.h'			\
+  'share/pal_statistics/cmake/pal_statisticsConfig.cmake'	\
+  'share/pal_statistics/package.xml:/<version>/s/[^0-9.]//gp'	\
+  'lib/pkgconfig/pal_statistics.pc:/Version/s/[^0-9.]//gp'	\
+  '${PYTHON_SYSLIBSEARCH}/pal_statistics/__init__.py'
+
+endif # PY_PAL_STATISTICS_DEPEND_MK ---------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

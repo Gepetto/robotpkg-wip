d9e8342dab0de1310cd3f17411da67f01157e722
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-03-23 16:09:34 +0100

    [wip/{py-,}sot-pattern-generator] Update to v3.2.3
    Changes since v3.2.2:
    - fix imports for python 3
    - fix sot-dynamic-pinocchio python lib use
    - renammed test dir
    - [cmake] CmakeLists.txt modified with new rules for compiling
    - CMake: export project and use exports from dependencies
    - CMake: keep minimal required instructions
    
    Packaging changes:
    - removed patches aa, obsolete

--- sot-pattern-generator-v3/depend.mk
+++ sot-pattern-generator-v3/depend.mk
@@ -6,19 +6,20 @@ DEPEND_DEPTH:=				${DEPEND_DEPTH}+
 SOT_PATTERN_GENERATOR_V3_DEPEND_MK:=	${SOT_PATTERN_GENERATOR_V3_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=			sot-pattern-generator-v3
+DEPEND_PKG+=				sot-pattern-generator-v3
 endif
 
 ifeq (+,$(SOT_PATTERN_GENERATOR_V3_DEPEND_MK)) # ------------------------------
 
-DEPEND_USE+=			sot-pattern-generator-v3
+DEPEND_USE+=				sot-pattern-generator-v3
 PREFER.sot-pattern-generator-v3?=	robotpkg
 
 DEPEND_ABI.sot-pattern-generator-v3?=	sot-pattern-generator-v3>=2.10
 DEPEND_DIR.sot-pattern-generator-v3?=	../../wip/sot-pattern-generator-v3
 SYSTEM_SEARCH.sot-pattern-generator-v3=\
-	include/sot-pattern-generator/pg.h				\
-	lib/plugin/pg.so						\
+	'include/sot/pattern-generator/config.hh:/SOT_PATTERN_GENERATOR_VERSION /s/[^0-9.]//gp'				\
+	'lib/dynamic-graph-plugins/libpg.so'										\
+	'lib/cmake/sot-pattern-generator/sot-pattern-generatorConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
 	'lib/pkgconfig/sot-pattern-generator.pc:/Version/s/[^0-9.]//pg'
 
 include ../../mk/sysdep/python.mk

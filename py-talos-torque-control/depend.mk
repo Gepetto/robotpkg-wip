c6b290d2260101bf5f8145241be3434c0d08304b
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-03-25 17:26:10 +0100

    [wip/{py,}talos-torque-control] Initial import

---
+++ py-talos-torque-control/depend.mk
@@ -0,0 +1,26 @@
+# robotpkg depend.mk for:	wip/py38-talos-torque-control
+# Created:			Guilhem Saurel on Wed, 25 Mar 2020
+#
+
+DEPEND_DEPTH:=				${DEPEND_DEPTH}+
+PY_TALOS_TORQUE_CONTROL_DEPEND_MK:=	${PY_TALOS_TORQUE_CONTROL_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=				sot-torque-control
+endif
+
+ifeq (+,$(PY_TALOS_TORQUE_CONTROL_DEPEND_MK)) # -------------------------------------------
+
+PREFER.py-talos-torque-control?=	robotpkg
+
+SYSTEM_SEARCH.py-talos-torque-control=\
+	'${PYTHON_SYSLIBSEARCH}/dynamic_graph/sot/torque_control/talos/__init__.py'
+
+DEPEND_USE+=				py-talos-torque-control
+
+DEPEND_ABI.py-talos-torque-control?=	${PKGTAG.python-}talos-torque-control>=1.0.0
+DEPEND_DIR.py-talos-torque-control?=	../../wip/py-talos-torque-control
+
+endif # PY_TALOS_TORQUE_CONTROL_DEPEND_MK -------------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

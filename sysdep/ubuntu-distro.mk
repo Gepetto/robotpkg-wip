bd06f344791198b15987aa40cd7d6a282fc8d4be
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-03-27 00:17:50 +0100

    [wip/sysdep/ubuntu-distro] delete

--- sysdep/ubuntu-distro.mk
+++
@@ -1,8 +0,0 @@
-# robotpkg wip/
-# Created:			Olivier Stasse on Thu 2 Jan 2020
-#
-ifndef _ubuntudistro_mk
-  _ubuntudistro_mk:=defined
-  _ubuntudistro:=$(shell ${SHELL} ${ROBOTPKG_DIR}/wip/sysdep/ubuntu-distro.sh)
-  export UBUNTUDISTRO:=  $(word 1,${_ubuntudistro})
-endif # UBUNTU_DISTRO_ROS_FLAVOR

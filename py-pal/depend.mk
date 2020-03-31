c487316446e22a4e87146d6495e867c2a1e927fc
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-04-01 00:40:36 +0200

    [wip/py-pal] rework
    - don't use PKGBASE
    - use PKGTAG
    - add missing dependency
    - cosmetic changes

--- py-pal/depend.mk
+++ py-pal/depend.mk
@@ -3,7 +3,7 @@
 #
 
 DEPEND_DEPTH:=			${DEPEND_DEPTH}+
-PY_PAL_DEPEND_MK:=	${PY_PAL_DEPEND_MK}+
+PY_PAL_DEPEND_MK:=		${PY_PAL_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
 DEPEND_PKG+=			py-pal
@@ -15,14 +15,14 @@ PREFER.py-pal?=	robotpkg
 
 DEPEND_USE+=			py-pal
 
-DEPEND_ABI.py-pal?=\
-	pal-python>=0.9.14os1
-DEPEND_DIR.py-pal?=\
-	../../wip/py-pal
+DEPEND_ABI.py-pal?=		${PKGTAG.python-}pal-python>=0.9.14os1
+DEPEND_DIR.py-pal?=		../../wip/py-pal
 
-SYSTEM_SEARCH.py-pal=\
-  '${PYTHON_SYSLIBSEARCH}/pal_python/pal_common.py' \
-  '${PYTHON_SYSLIBSEARCH}/pal_python/pal_launch.py' 
+SYSTEM_SEARCH.py-pal=					\
+  '${PYTHON_SYSLIBSEARCH}/pal_python/pal_common.py'	\
+  '${PYTHON_SYSLIBSEARCH}/pal_python/pal_launch.py'
+
+include ../../mk/sysdep/python.mk
 
 endif # PY_PAL_DEPEND_MK ---------------------------------------
 

d718a2b40a2fa970ae6ea51b9b8c209280aa3be1
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-04 17:14:43 +0200

    [wip/hpp-affordance-corba] update SYSTEM_SEARCH

--- py-hpp-affordance-corba/depend.mk
+++ py-hpp-affordance-corba/depend.mk
@@ -1,4 +1,4 @@
-# robotpkg depend.mk for:	wip/py-hpp-affordance-corba-corba
+# robotpkg depend.mk for:	wip/py-hpp-affordance-corba
 # Created:			Guilhem Saurel on Fri, 4 Jan 2019
 #
 
@@ -17,12 +17,13 @@ PREFER.py-hpp-affordance-corba?=	robotpkg
 
 DEPEND_USE+=		py-hpp-affordance-corba
 
-DEPEND_ABI.py-hpp-affordance-corba?=	${PKGTAG.python-}hpp-affordance-corba>=4.7.0
+DEPEND_ABI.py-hpp-affordance-corba?=	${PKGTAG.python-}hpp-affordance-corba>=4.9.0
 DEPEND_DIR.py-hpp-affordance-corba?=	../../wip/py-hpp-affordance-corba
 
-SYSTEM_SEARCH.py-hpp-affordance-corba=\
-	include/hpp/corbaserver/affordance/config.hh			\
-	lib/hppPlugins/affordance-corba.so				\
+SYSTEM_SEARCH.py-hpp-affordance-corba=										\
+	'include/hpp/corbaserver/affordance/config.hh'								\
+	'lib/cmake/hpp-affordance-corba/hpp-affordance-corbaConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
+	'lib/hppPlugins/affordance-corba.so'									\
 	'lib/pkgconfig/hpp-affordance-corba.pc:/Version/s/[^0-9.]//gp'
 
 endif # PY_HPP_AFFORDANCE_CORBA_DEPEND_MK --------------------------------------------

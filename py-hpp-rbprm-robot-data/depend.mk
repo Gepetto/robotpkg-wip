2065927d99401dc8357cdf826e69b4c3e07a96ad
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-04 17:05:49 +0200

    [wip/py-hpp-rbprm-robot-data] update SYSTEM_SEARCH

--- py-hpp-rbprm-robot-data/depend.mk
+++ py-hpp-rbprm-robot-data/depend.mk
@@ -17,11 +17,12 @@ PREFER.py-hpp-rbprm-robot-data?=	robotpkg
 
 DEPEND_USE+=		py-hpp-rbprm-robot-data
 
-DEPEND_ABI.py-hpp-rbprm-robot-data?=	${PKGTAG.python-}hpp-rbprm-robot-data>=4.7.0
+DEPEND_ABI.py-hpp-rbprm-robot-data?=	${PKGTAG.python-}hpp-rbprm-robot-data>=4.9.0
 DEPEND_DIR.py-hpp-rbprm-robot-data?=	../../wip/py-hpp-rbprm-robot-data
 
-SYSTEM_SEARCH.py-hpp-rbprm-robot-data=\
-	include/hpp/rbprm-robot-data/config.hh	\
+SYSTEM_SEARCH.py-hpp-rbprm-robot-data=										\
+	'include/hpp/rbprm-robot-data/config.hh'								\
+	'lib/cmake/hpp-rbprm-robot-data/hpp-rbprm-robot-dataConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
 	'lib/pkgconfig/hpp-rbprm-robot-data.pc:/Version/s/[^0-9.]//gp'
 
 endif # PY_HPP_RBPRM_ROBOT_DATA_DEPEND_MK --------------------------------------------

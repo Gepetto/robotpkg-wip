4d13c1d4ed7009b29fbc449567935293d6e018b7
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-04 17:34:29 +0200

    [wip/py-hpp-hrp2] update SYSTEM_SEARCH

--- py-hpp-hrp2/depend.mk
+++ py-hpp-hrp2/depend.mk
@@ -2,26 +2,27 @@
 # Created:			Guilhem Saurel on Tue,  8 Oct 2019
 #
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH}+
-PY_HPP_HRP2_DEPEND_MK:=	${PY_HPP_HRP2_DEPEND_MK}+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+PY_HPP_HRP2_DEPEND_MK:=		${PY_HPP_HRP2_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		py-hpp-hrp2
+DEPEND_PKG+=			py-hpp-hrp2
 endif
 
 ifeq (+,$(PY_HPP_HRP2_DEPEND_MK)) # --------------------------------------
 
 include ../../mk/sysdep/python.mk
 
-PREFER.py-hpp-hrp2?=	robotpkg
+PREFER.py-hpp-hrp2?=		robotpkg
 
-DEPEND_USE+=		py-hpp-hrp2
+DEPEND_USE+=			py-hpp-hrp2
 
-DEPEND_ABI.py-hpp-hrp2?=	${PKGTAG.python-}hpp-hrp2>=4.7.0
+DEPEND_ABI.py-hpp-hrp2?=	${PKGTAG.python-}hpp-hrp2>=4.9.0
 DEPEND_DIR.py-hpp-hrp2?=	../../wip/py-hpp-hrp2
 
-SYSTEM_SEARCH.py-hpp-hrp2=\
-	include/hpp/hrp2/config.hh	\
+SYSTEM_SEARCH.py-hpp-hrp2=								\
+	'include/hpp/hrp2/config.hh'							\
+	'lib/cmake/hpp-hrp2/hpp-hrp2ConfigVersion.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'	\
 	'lib/pkgconfig/hpp-hrp2.pc:/Version/s/[^0-9.]//gp'
 
 endif # PY_HPP_HRP2_DEPEND_MK --------------------------------------------

85934832149473dc63e7b756613df351701f4d4e
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-11 13:44:16 +0200

    [wip/py-sot-talos-balance] fix SYSTEM_SEARCH

--- py-sot-talos-balance/depend.mk
+++ py-sot-talos-balance/depend.mk
@@ -14,8 +14,7 @@ ifeq (+,$(PY_SOT_TALOS_BALANCE_DEPEND_MK)) # -----------------------------------
 PREFER.py-sot-talos-balance?=	robotpkg
 
 SYSTEM_SEARCH.py-sot-talos-balance=\
-  '${PYTHON_SYSLIBSEARCH}/dynamic_graph/sot/talos_balance/inverse_dynamics_balance_controller/wrap.so' \
-  '${PYTHON_SYSLIBSEARCH}/dynamic_graph/sot/talos_balance/__init__.py'
+  '${PYTHON_SYSLIBSEARCH}/sot_talos_balance/__init__.py'
 
 DEPEND_USE+=		py-sot-talos-balance
 

acc4176627db96172e9a7ce19e68f85bfd75847e
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-12 20:25:15 +0200

    [wip/py-multiprocess] fix PLIST

--- py-multiprocess/depend.mk
+++ py-multiprocess/depend.mk
@@ -21,7 +21,7 @@ DEPEND_ABI.py-multiprocess?=	${PKGTAG.python-}multiprocess>=0.6.1
 DEPEND_DIR.py-multiprocess?=	../../wip/py-multiprocess
 
 SYSTEM_SEARCH.py-multiprocess=	\
-  '${PYTHON_SYSLIBSEARCH}/multiprocess*.egg'
+  '${PYTHON_SYSLIBSEARCH}/multiprocess/__init__.py'
 
 endif # PY_MULTIPROCESS_DEPEND_MK ----------------------------------------------
 

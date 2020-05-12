6c436aaefa89466d2b0c3911d55fa0b21980a41f
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-12 13:42:39 +0200

    [wip/py-cvxpy] Initial import

---
+++ py-cvxpy/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	optimization/py38-cvxpy
+# Created:			Guilhem Saurel on Tue, 12 May 2020
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+PY_CVXPY_DEPEND_MK:=		${PY_CVXPY_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			py-cvxpy
+endif
+
+ifeq (+,$(PY_CVXPY_DEPEND_MK)) # ----------------------------------------
+
+include ../../mk/sysdep/python.mk
+
+PREFER.py-cvxpy?=		robotpkg
+
+DEPEND_USE+=			py-cvxpy
+
+DEPEND_ABI.py-cvxpy?=		${PKGTAG.python-}cvxpy>=1.0.31
+DEPEND_DIR.py-cvxpy?=		../../wip/py-cvxpy
+
+SYSTEM_SEARCH.py-cvxpy=	\
+  '${PYTHON_SITELIB}/cvxpy*.egg'
+
+endif # PY_CVXPY_DEPEND_MK ----------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

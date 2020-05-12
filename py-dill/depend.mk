02c7b74974957b1d84e1f246845029e848bb07dd
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-05-12 13:11:01 +0200

    [wip/py-dill] Initial import

---
+++ py-dill/depend.mk
@@ -0,0 +1,28 @@
+# robotpkg depend.mk for:	wip/py38-dill
+# Created:			Guilhem Saurel on Tue, 12 May 2020
+#
+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
+PY_DILL_DEPEND_MK:=		${PY_DILL_DEPEND_MK}+
+
+ifeq (+,$(DEPEND_DEPTH))
+DEPEND_PKG+=			py-dill
+endif
+
+ifeq (+,$(PY_DILL_DEPEND_MK)) # ----------------------------------------
+
+include ../../mk/sysdep/python.mk
+
+PREFER.py-dill?=		robotpkg
+
+DEPEND_USE+=			py-dill
+
+DEPEND_ABI.py-dill?=		${PKGTAG.python-}dill>=0.3.1.1
+DEPEND_DIR.py-dill?=		../../wip/py-dill
+
+SYSTEM_SEARCH.py-dill=		\
+  '${PYTHON_SITELIB}/dill*.egg'
+
+endif # PY_DILL_DEPEND_MK ----------------------------------------------
+
+DEPEND_DEPTH:=		${DEPEND_DEPTH:+=}

d9e2b2e030315b6f45a00734a2e2060df72d2d71
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-06-08 17:52:15 +0200

    [wip/py-prf-gazebo-ros-pkgs] fix SYSTEM_SEARCH
    As the headers are not in pal-erbium-gazebo-ros-control

--- py-prf-gazebo-ros-pkgs/depend.mk
+++ py-prf-gazebo-ros-pkgs/depend.mk
@@ -27,9 +27,6 @@ endif
 DEPEND_DIR.py-prf-gazebo-ros-pkgs?=	../../wip/py-prf-gazebo-ros-pkgs
 
 SYSTEM_SEARCH.py-prf-gazebo-ros-pkgs=					\
-  'include/gazebo_ros_control/gazebo_ros_control_plugin.h'		\
-  'include/gazebo_ros_control/default_robot_hw_sim.h'			\
-  'include/gazebo_ros_control/robot_hw_sim.h'				\
   'share/gazebo_ros_control/cmake/gazebo_ros_controlConfig.cmake'	\
   'share/gazebo_ros_control/package.xml:/<version>/s/[^0-9.]//gp'	\
   'lib/pkgconfig/gazebo_ros_control.pc:/Version/s/[^0-9.]//gp'		\

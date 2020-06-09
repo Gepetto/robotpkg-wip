20c3c20929ed8a60ff842d7fc9c37bc8b01542c5
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-06-09 13:34:59 +0200

    [wip/py-prf-ros-controllers] fix SYSTEM_SEARCH for erbium

--- py-prf-ros-controllers/depend.mk
+++ py-prf-ros-controllers/depend.mk
@@ -19,12 +19,10 @@ DEPEND_ABI.py-prf-ros-controllers?=	${PKGTAG.python-}prf-ros-controllers>=0.2.8
 DEPEND_DIR.py-prf-ros-controllers?=	../../wip/py-prf-ros-controllers
 
 SYSTEM_SEARCH.py-prf-ros-controllers=\
-  'include/joint_state_controller/joint_state_controller.h'		\
   'lib/libjoint_state_controller.so'					\
   'share/joint_state_controller/package.xml:/<version>/s/[^0-9.]//gp'	\
   'lib/pkgconfig/joint_state_controller.pc:/Version/s/[^0-9.]//gp'	\
-  'share/controller_interface/cmake/controller_interfaceConfig.cmake'	\
-  '${PYTHON_SYSLIBSEARCH}/diff_drive_controller/__init__.py'
+  'share/controller_interface/cmake/controller_interfaceConfig.cmake'
 
 include ../../mk/sysdep/python.mk
 

524218fff2b5842b376cea9aa9d9da6042f41663
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-07-28 23:56:27 +0200

    [wip/{py-,}example-adder] Update to v3.0.0
    Changes in v3.0.0:
    - rewrite project

--- py-example-adder/depend.mk
+++ py-example-adder/depend.mk
@@ -2,25 +2,24 @@
 # Created:			Guilhem Saurel on Tue, 10 Jul 2018
 #
 
-DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+DEPEND_DEPTH:=			${DEPEND_DEPTH}+
 PY_EXAMPLE_ADDER_DEPEND_MK:=	${PY_EXAMPLE_ADDER_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=		py-example-adder
+DEPEND_PKG+=			py-example-adder
 endif
 
 ifeq (+,$(EXAMPLE_ADDER_DEPEND_MK)) # ------------------------------------------
 
 PREFER.py-example-adder?=	robotpkg
 
-#TODO
-SYSTEM_SEARCH.py-example-adder=\
-  '${PYTHON_SYSLIBSEARCH}/example_adder/libexample-adder.so'							\
+SYSTEM_SEARCH.py-example-adder=					\
+  '${PYTHON_SYSLIBSEARCH}/example_adder/libexample-adder.so'	\
   '${PYTHON_SYSLIBSEARCH}/example_adder/__init__.py'
 
-DEPEND_USE+=		py-example-adder
+DEPEND_USE+=			py-example-adder
 
-DEPEND_ABI.py-example-adder?=	${PKGTAG.python-}example-adder>=1.0.0
+DEPEND_ABI.py-example-adder?=	${PKGTAG.python-}example-adder>=3.0.0
 DEPEND_DIR.py-example-adder?=	../../wip/py-example-adder
 
 endif # PY_EXAMPLE_ADDER_DEPEND_MK ------------------------------------------------

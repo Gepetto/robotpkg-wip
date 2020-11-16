65df273c820a4fb0cbf2e1069d1880c30710664d
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-11-16 18:11:38 +0100

    [wip] agimus_sot_msgs → agimus-sot-msgs
    Affected packages:
    - wip/agimus
    - wip/agimus-hpp
    - wip/agimus-sot

--- agimus-sot-msgs/depend.mk
+++ agimus-sot-msgs/depend.mk
@@ -1,4 +1,4 @@
-# robotpkg depend.mk for:	wip/agimus_sot_msgs
+# robotpkg depend.mk for:	wip/agimus-sot-msgs
 # Created:			Long Ha Thuc on Wed, 19 Aug 2020
 #
 
@@ -6,19 +6,19 @@ DEPEND_DEPTH:=			${DEPEND_DEPTH}+
 AGIMUS_SOT_MSGS_DEPEND_MK:=	        ${AGIMUS_SOT_MSGS_DEPEND_MK}+
 
 ifeq (+,$(DEPEND_DEPTH))
-DEPEND_PKG+=			agimus_sot_msgs
+DEPEND_PKG+=			agimus-sot-msgs
 endif
 
 ifeq (+,$(AGIMUS_SOT_MSGS_DEPEND_MK)) # -------------------------------------------
 
-PREFER.agimus_sot_msgs=		robotpkg
+PREFER.agimus-sot-msgs=		robotpkg
 
-DEPEND_USE+=			agimus_sot_msgs
+DEPEND_USE+=			agimus-sot-msgs
 
-DEPEND_ABI.agimus_sot_msgs?=	agimus_sot_msgs>=1.0.0
-DEPEND_DIR.agimus_sot_msgs?=	../../wip/agimus_sot_msgs
+DEPEND_ABI.agimus-sot-msgs?=	agimus-sot-msgs>=1.0.0
+DEPEND_DIR.agimus-sot-msgs?=	../../wip/agimus-sot-msgs
 
-SYSTEM_SEARCH.agimus_sot_msgs= \
+SYSTEM_SEARCH.agimus-sot-msgs= \
 	'lib/pkgconfig/agimus_sot_msgs.pc:/Version/s/[^0-9.]//gp' \
 	'share/agimus_sot_msgs/cmake/agimus_sot_msgsConfig-version.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp' \
     'share/agimus_sot_msgs/cmake/agimus_sot_msgsConfig.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'

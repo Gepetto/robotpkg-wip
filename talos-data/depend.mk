60710ed191ce0a9bd149694d954b1ee9342aa318
Guilhem Saurel <guilhem.saurel@laas.fr>
2020-11-19 16:26:49 +0100

    [wip/talos-data] Update to v2.0.0
    Changes in v2.0.0:
    - removed gazebo pluging, now available in https://github.com/Gepetto/gazebo-spring

--- talos-data/depend.mk
+++ talos-data/depend.mk
@@ -16,13 +16,12 @@ PREFER.talos-data?=	robotpkg
 SYSTEM_SEARCH.talos-data=\
 	'include/talos/data/config.hh:/TALOS_DATA_VERSION /s/[^0-9.]//gp'			\
 	'lib/cmake/talos_data/talos_dataConfigVersion.cmake:/PACKAGE_VERSION /s/[^0-9.]//gp'	\
-	'lib/libtalos_data.so'									\
 	'lib/pkgconfig/talos_data.pc:/Version/s/[^0-9.]//gp'					\
 	'share/talos_data/urdf/talos_reduced.urdf'
 
 DEPEND_USE+=		talos-data
 
-DEPEND_ABI.talos-data?=	talos-data>=1.1.0
+DEPEND_ABI.talos-data?=	talos-data>=2.0.0
 DEPEND_DIR.talos-data?=	../../wip/talos-data
 
 endif # TALOS_DATA_DEPEND_MK -------------------------------------------------

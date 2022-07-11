e0ddcfef44de11525ef78154e9eac1ec6201bddc
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-07-11 12:49:40 +0200

    [wip/ouster-gazebo-simulation] Update to v2.1.2
    Changes in v2.1.2:
    - set noise stddev > 0
    - fix auto headers location
    - reformat

--- ouster-gazebo-simulation/depend.mk
+++ ouster-gazebo-simulation/depend.mk
@@ -18,7 +18,7 @@ DEPEND_ABI.ouster-gazebo-simulation?=	ouster-gazebo-simulation>=2.1.0
 DEPEND_DIR.ouster-gazebo-simulation?=	../../wip/ouster-gazebo-simulation
 
 SYSTEM_SEARCH.ouster-gazebo-simulation=\
-  'include/ouster/gazebo/simulation/config.hpp:/OUSTER_GAZEBO_SIMULATION_VERSION /s/[^0-9.]//gp'		\
+  'include/ouster-gazebo-simulation/config.hpp:/OUSTER_GAZEBO_SIMULATION_VERSION /s/[^0-9.]//gp'		\
   'share/ouster_os1_64/package.xml:/<version>/s/[^0-9.]//gp'							\
   'share/ouster_gazebo_plugins/cmake/ouster_gazebo_pluginsConfig-version.cmake:/PACKAGE_VERSION/s/[^0-9.]//gp'
 
